05c6f74c621a0b601bd1281004bf1ba7ef4de105
Sergey Miryanov <sergey.miryanov@bs_bos_core.com>
2009-08-31 14:43:50 +0600

    Edit: Add BS_LOG_USE_BUFFER
    Set BS_LOG_USE_BUFFER to 0 to disable buffered output in release
    Also add BS_LOG_USE_BUFFER_FORCE to 1 to enable buffered output in debug

--- kernel/src/bs_log_scribers.cpp
+++ kernel/src/bs_log_scribers.cpp
@@ -12,8 +12,27 @@ namespace blue_sky {
 namespace log {
 namespace detail {
 
+#ifdef _DEBUG
+#ifdef BS_LOG_USE_BUFFER
+#undef BS_LOG_USE_BUFFER
+#endif
+#define BS_LOG_USE_BUFFER 0
+#endif
+
+#ifndef BS_LOG_USE_BUFFER
+#define BS_LOG_USE_BUFFER 1
+#endif
+
+#ifndef BS_LOG_USE_BUFFER_FORCE
+#define BS_LOG_USE_BUFFER_FORCE 0
+#endif
+
+#if BS_LOG_USE_BUFFER_FORCE
+#define BS_LOG_USE_BUFFER 1
+#endif
+
 	void cout_scriber::write(const std::string &str) const {
-#ifndef _DEBUG
+#if BS_LOG_USE_BUFFER
     static bool is_buffer_installed = false;
     if (!is_buffer_installed)
       {
