70bf38560b0d2c3fe1a76bbb96069483d86e8f4e
uentity <unientity@gmail.com>
2012-02-22 09:30:57 +0600

    common: remove deprecated scripts

--- bs_script_configure_and_make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script configures blue-sky and makes it
-
-autoreconf --install
-./configure --with-boost=/opt/boost #--with-boost=/your/boost/path (without "boost_1_34_1" like)
-#export BS_PYTHON_INCLUDE=-I/usr/include/python2.5 #/your/python/path (like /usr/lib/python2.5)
-#export BS_LIBCONFIGPP_CFLAGS=`pkg-config libconfig++ --cflags`
-#export BS_LIBCONFIGPP_LIBS=`pkg-config libconfig++ --libs`
-#make clean && make
