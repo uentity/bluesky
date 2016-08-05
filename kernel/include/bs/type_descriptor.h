cbbc715d32922760c8df9216a413ff525e72ba6d
uentity <unientity@gmail.com>
2016-08-05 17:33:20 +0500

    type_descriptor: optimize & simplify constructors, fix some corner cases

--- kernel/include/bs/type_descriptor.h
+++ kernel/include/bs/type_descriptor.h
@@ -15,7 +15,6 @@
 
 #include <set>
 #include <list>
-#include <memory>
 #include <boost/preprocessor/seq/for_each.hpp>
 
 #define BS_NIL_TYPE_TAG "__blue_sky_nil_type__"
@@ -27,6 +26,9 @@ typedef const std::shared_ptr< objbase >& bs_type_cpy_ctor_param;
 typedef objbase* (*BS_TYPE_CREATION_FUN)(bs_type_ctor_param);
 typedef objbase* (*BS_TYPE_COPY_FUN)(bs_type_cpy_ctor_param);
 typedef blue_sky::type_descriptor (*BS_GET_TD_FUN)();
+
+typedef std::set< std::shared_ptr< objbase > > bs_objinst_holder;
+typedef std::shared_ptr< bs_objinst_holder > sp_objinst;
 
 template< class BST > struct gen_type_descriptor;
 
@@ -51,7 +53,7 @@ private:
 	template< class T, int nocopy >
 	struct extract_copyfn {
 		static BS_TYPE_COPY_FUN go() {
-			return NULL;
+			return nullptr;
 		}
 	};
 
@@ -62,48 +64,75 @@ private:
 		}
 	};
 
+	template< class T, class unused = void >
+	struct extract_tdfun {
+		static BS_GET_TD_FUN go() {
+			return T::bs_type;
+		}
+	};
+	template < class unused >
+	struct extract_tdfun< nil, unused > {
+		static BS_GET_TD_FUN go() {
+			return nullptr;
+		}
+	};
+
+	template< class T>
+	struct extract_typename {
+		template< class str_type >
+		static std::string go(str_type val) {
+			return val;
+		}
+
+		static std::string go(std::nullptr_t) {
+			return bs_type_name< T >();
+		}
+	};
+
 public:
 	// default constructor - type_descriptor points to nil
 	type_descriptor()
 		:
-		bs_ti_(nil_type_info()), creation_fun_(NULL), copy_fun_(NULL), parent_td_fun_(NULL),
+		bs_ti_(nil_type_info()), creation_fun_(nullptr), copy_fun_(nullptr), parent_td_fun_(nullptr),
 		type_name_(BS_NIL_TYPE_TAG)
 	{}
 
 	// Nil constructor for temporary tasks (searching etc)
 	type_descriptor(const std::string& stype)
 		:
-		bs_ti_(nil_type_info()), creation_fun_(NULL), copy_fun_(NULL), parent_td_fun_(NULL),
+		bs_ti_(nil_type_info()), creation_fun_(nullptr), copy_fun_(nullptr), parent_td_fun_(nullptr),
 		type_name_(stype)
 	{}
 
 	// standard constructor
 	type_descriptor(
 		const BS_TYPE_INFO& ti, const BS_TYPE_CREATION_FUN& cr_fn, const BS_TYPE_COPY_FUN& cp_fn,
-		const BS_GET_TD_FUN& parent_td_fn, const std::string& type_name, const std::string& description
+		const BS_GET_TD_FUN& parent_td_fn, const char* type_name, const char* description
 	) :
 		bs_ti_(ti), creation_fun_(cr_fn), copy_fun_(cp_fn), parent_td_fun_(parent_td_fn),
 		type_name_(type_name), description_(description)
 	{}
 
 	// templated ctor for BlueSky types
-	template< class T, class base = nil, int nocopy = 0 >
-	type_descriptor(Loki::Type2Type< T > /* this_t */, Loki::Type2Type< base > /* base_t */,
-					Loki::Int2Type< nocopy > /* no_copy_fun */,
-					const std::string& type_name, const std::string& description = "")
-		:
-		bs_ti_(BS_GET_TI(T)), creation_fun_(T::bs_create_instance),
-		copy_fun_(extract_copyfn< T, nocopy >::go()), parent_td_fun_(base::bs_type),
-		type_name_(type_name), description_(description)
-	{}
+	//template< class T, class base = nil, int nocopy = 0 >
+	//type_descriptor(Loki::Type2Type< T > /* this_t */, Loki::Type2Type< base > /* base_t */,
+	//				Loki::Int2Type< nocopy > /* no_copy_fun */,
+	//				const char* type_name = nullptr, const char* description = nullptr)
+	//	:
+	//	bs_ti_(BS_GET_TI(T)), creation_fun_(T::bs_create_instance),
+	//	copy_fun_(extract_copyfn< T, nocopy >::go()),
+	//	parent_td_fun_(extract_tdfun< base >::go()),
+	//	type_name_(extract_typename< T >::go(type_name)), description_(description)
+	//{}
 
-	// express ctor for BlueSky types with type name auto-deducing
-	template< class T, class base = nil, int nocopy = 0 >
-	type_descriptor()
+	// templated ctor for BlueSky types
+	template< class T, class base = nil, int nocopy = 0, class typename_t = std::nullptr_t >
+	type_descriptor(typename_t type_name = nullptr, const char* description = nullptr)
 		:
 		bs_ti_(BS_GET_TI(T)), creation_fun_(T::bs_create_instance),
-		copy_fun_(extract_copyfn< T, nocopy >::go()), parent_td_fun_(base::bs_type),
-		type_name_(bs_type_name< T >())
+		copy_fun_(extract_copyfn< T, nocopy >::go()),
+		parent_td_fun_(extract_tdfun< base >::go()),
+		type_name_(extract_typename< T >::go(type_name)), description_(description)
 	{}
 
 	/// read access to base fields
@@ -126,7 +155,7 @@ public:
 		return ::blue_sky::is_nil(bs_ti_);
 	}
 	bool is_copyable() const {
-		return (copy_fun_ != NULL);
+		return (copy_fun_ != nullptr);
 	}
 
 	/// conversions
