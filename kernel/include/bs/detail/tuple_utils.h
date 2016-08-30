192c3f3277c7b62cc38e37df5720caa96d95d66a
uentity <unientity@gmail.com>
2016-08-30 20:05:33 +0500

    common: implement some std::tuple related helpers
    invoke() and invoke_range() -- call specified function with arguments
    passed as std::tuple.
    subtuple() -- extract range of tuple elements into new tuple.

---
+++ kernel/include/bs/detail/tuple_utils.h
@@ -0,0 +1,103 @@
+/// @file
+/// @author uentity
+/// @date 26.08.2016
+/// @brief Misc std::tuple related helpers
+/// @copyright
+/// This Source Code Form is subject to the terms of the Mozilla Public License,
+/// v. 2.0. If a copy of the MPL was not distributed with this file,
+/// You can obtain one at https://mozilla.org/MPL/2.0/
+
+#pragma once
+
+#include <tuple>
+#include <utility>
+#include <type_traits>
+
+namespace blue_sky {
+
+/*-----------------------------------------------------------------
+ * Helper to generaate integer sequence in given bounds
+ *----------------------------------------------------------------*/
+template< std::size_t From, std::size_t To >
+struct make_bounded_sequence {
+	template< std::size_t offset, std::size_t... Ints >
+	decltype(auto) static constexpr offset_sequence(
+		std::integral_constant< std::size_t, offset >, std::index_sequence< Ints... >
+	) {
+		return std::index_sequence< Ints + offset... >{};
+	}
+
+	static constexpr auto value = offset_sequence(
+		std::integral_constant< std::size_t, From >(),
+		std::make_index_sequence< To - From >{}
+	);
+};
+
+/*-----------------------------------------------------------------
+ * invoke given function with arguments unpacked from tuple
+ *----------------------------------------------------------------*/
+template<typename Func, typename Tup, std::size_t... index>
+decltype(auto) invoke_helper(Func&& func, Tup&& tup, std::index_sequence<index...>) {
+	return func(std::get<index>(std::forward<Tup>(tup))...);
+}
+
+template<typename Func, typename Tup>
+decltype(auto) invoke(Func&& func, Tup&& tup) {
+	constexpr auto Size = std::tuple_size< std::decay_t<Tup> >::value;
+	return invoke_helper(
+		std::forward<Func>(func),
+		std::forward<Tup>(tup),
+		std::make_index_sequence<Size>{}
+	);
+}
+
+template<
+	typename Func, typename Tup, std::size_t From = 0,
+	std::size_t To = std::tuple_size< std::decay_t< Tup > >::value
+>
+decltype(auto) invoke_range(
+	Func&& func, Tup&& tup,
+	std::integral_constant< std::size_t, From > = std::integral_constant< std::size_t, 0 >(),
+	std::integral_constant< std::size_t, To > = std::integral_constant<
+		std::size_t, std::tuple_size< std::decay_t< Tup > >::value
+	>()
+) {
+	return invoke_helper(
+		std::forward<Func>(func),
+		std::forward<Tup>(tup),
+		make_bounded_sequence< From, To >::value
+	);
+}
+
+/*-----------------------------------------------------------------
+ * Helper to extract range of tuple args as new tuple
+ *----------------------------------------------------------------*/
+template< typename Tup, std::size_t... index >
+decltype(auto) subtuple_helper(Tup&& tup, std::index_sequence<index...>) {
+	// use explicit tuple construction in order to save exact information about elem types
+	return std::tuple< std::tuple_element_t< index, std::decay_t< Tup > >... >(
+		std::get< index >(std::forward< Tup >(tup))...
+	);
+	// disabled -- loose exact types info
+	//return std::forward_as_tuple(std::get<index>(std::forward<Tup>(tup))...);
+}
+
+template<
+	typename Tup, std::size_t From = 0,
+	std::size_t To = std::tuple_size< std::decay_t< Tup > >::value
+>
+decltype(auto) subtuple(
+	Tup&& tup,
+	std::integral_constant< std::size_t, From > from = std::integral_constant< std::size_t, 0 >(),
+	std::integral_constant< std::size_t, To > to = std::integral_constant<
+		std::size_t, std::tuple_size< std::decay_t< Tup > >::value
+	>()
+) {
+	return subtuple_helper(
+		std::forward<Tup>(tup),
+		make_bounded_sequence< From, To >::value
+	);
+}
+
+} /* namespace blue_sky */
+
