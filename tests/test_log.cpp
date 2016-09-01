955315e1493a0a1536201c3d12324d16d5ef2873
uentity <unientity@gmail.com>
2016-09-01 19:09:27 +0500

    log: add log subsystem unit-test

---
+++ tests/test_log.cpp
@@ -0,0 +1,50 @@
+/// @file
+/// @author uentity
+/// @date 30.08.2016
+/// @brief BlueSky log subsystem test
+/// @copyright
+/// This Source Code Form is subject to the terms of the Mozilla Public License,
+/// v. 2.0. If a copy of the MPL was not distributed with this file,
+/// You can obtain one at https://mozilla.org/MPL/2.0/
+
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+#include <iostream>
+
+#include <bs/exception.h>
+#include <bs/log.h>
+
+struct example {
+	friend std::ostream& operator <<(std::ostream& os, const example& ex) {
+		return os << "hello";
+	}
+};
+
+BOOST_AUTO_TEST_CASE(test_bs_log) {
+	using namespace blue_sky;
+	using namespace blue_sky::log;
+
+	std::cout << "*** testing bs_log..." << std::endl;
+
+	//bsout().logger().info("{}", example());
+
+	auto bs_out = bs_log("out");
+	bs_out << info("Hello World! My name is {}", "Alexander") << end;
+	int m = 2;
+	bs_out << I("My age is {} years and {} months") << 35 << m << end;
+	bserr() << W("My age is {} years and {} months", 35, m) << end;
+	BSERROR << E("My age is {} years and {} months") << 35 << m << end;
+	bserr() << C("My age is {} years and {} months") << 35 << m << end;
+	bsout() << T("My age is {} years and {} months") << 35 << m << end;
+	BSOUT << D("My age is {} years and {} months") << 35 << m << end;
+	BSOUT << O("My age is {} years and {} months") << 35 << m << end;
+
+	auto&& tape = I("Hello {} {}") << std::string("World") << 35;
+	bsout() << tape << end;
+
+	bsout() << "Hello simple {}" << "World" << end;
+	bsout() << I("My name is {}") << "Alexander" << "and I'm {} years old" << 35 << end;
+
+	bs_exception("Kernel panic!", "Kernel::test").print();
+}
+
