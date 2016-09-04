12ae06d45aa5c7cb063076168d5602fc9c53a0a3
uentity <unientity@gmail.com>
2016-09-04 17:26:48 +0500

    bspy_loader: use pybind11

--- python/bspy_loader/src/main.cpp
+++ python/bspy_loader/src/main.cpp
@@ -13,10 +13,13 @@
 // You should have received a copy of the GNU General Public License
 // along with BlueSky; if not, see <http://www.gnu.org/licenses/>.
 
+#include <pybind11/pybind11.h>
+
 #include <bs/common.h>
 #include <bs/kernel.h>
 #include <bs/detail/lib_descriptor.h>
 #include <bs/log.h>
+
 #include <boost/preprocessor/cat.hpp>
 #include <boost/preprocessor/stringize.hpp>
 
@@ -33,13 +36,16 @@
 
 using namespace blue_sky;
 using namespace std;
+namespace py = pybind11;
 
-BS_C_API_PLUGIN void INIT_FN_NAME() {
+PYBIND11_PLUGIN(TARGET_NAME) {
+	auto m = py::module(S_TARGET_NAME);
+
 	//search for BlueSky's kernel plugin descriptor
 	BS_GET_PLUGIN_DESCRIPTOR get_pd_fn;
 	if(detail::lib_descriptor::load_sym_glob("bs_get_plugin_descriptor", get_pd_fn) != 0 || !get_pd_fn) {
 		BSERROR << log::E("BlueSky kernel descriptor wasn't found or invalid!") << log::end;
-		return;
+		return m.ptr();
 		//throw std::runtime_error("BlueSky kernel descriptor wasn't found");
 	}
 	plugin_descriptor* kernel_pd = get_pd_fn();
@@ -48,5 +54,7 @@ BS_C_API_PLUGIN void INIT_FN_NAME() {
 
 	//load plugins with Python subsystem
 	//give_kernel::Instance().LoadPlugins(true);
+
+	return m.ptr();
 }
 
