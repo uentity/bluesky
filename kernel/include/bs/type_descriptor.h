d649103b3e6dc5319eca6dd29f0b128afb7c11bd
uentity <unientity@gmail.com>
2016-10-10 09:12:43 +0500

    type_descriptor: initial variable arguments creation support for BS types
    Ideas.
    
    1) add_constructor() allows to register type's constructor with
    arbitrary arguments, or vanilla free fucntion that returns shared_ptr
    to new object (for ex. bs_create_instance()).
    
    2) Same applies to add_copy_constructor() -- type'c copy constructor or
    free function making copy can be registered. Argument for copy creation
    is fixed to const type l-value reference.
    
    3) Each creation method is stored as pair of void pointers: first
    points to stateless lambda that creates object, second point to
    free function or equal to nullptr if constructor is registered. Pairs
    are stored in unordered_map, where key is type of creator arguments
    tuple with decayed arguments types. So there will be no difference
    between char* and const char* arguments, etc.
    
    New API allows to register creation and copy functions after
    type_descriptor has been created.
    Code needs fixing, tuning and testing.

--- kernel/include/bs/type_descriptor.h
+++ kernel/include/bs/type_descriptor.h
@@ -13,6 +13,7 @@
 #include "type_info.h"
 //#include "type_macro.h"
 
+#include <unordered_map>
 #include <set>
 #include <list>
 #include <boost/preprocessor/seq/for_each.hpp>
@@ -21,11 +22,11 @@
 
 namespace blue_sky {
 
-typedef const std::shared_ptr< objbase >& bs_type_ctor_param;
-typedef const std::shared_ptr< objbase >& bs_type_cpy_ctor_param;
-typedef objbase* (*BS_TYPE_CREATION_FUN)(bs_type_ctor_param);
-typedef objbase* (*BS_TYPE_COPY_FUN)(bs_type_cpy_ctor_param);
-typedef blue_sky::type_descriptor (*BS_GET_TD_FUN)();
+typedef const std::shared_ptr< objbase > bs_type_ctor_result;
+typedef const std::shared_ptr< objbase >& bs_type_copy_param;
+//typedef objbase* (*BS_TYPE_CREATION_FUN)(const std::shared_ptr< objbase >&);
+typedef bs_type_ctor_result (*BS_TYPE_COPY_FUN)(bs_type_copy_param);
+typedef const blue_sky::type_descriptor& (*BS_GET_TD_FUN)();
 
 typedef std::set< std::shared_ptr< objbase > > bs_objinst_holder;
 typedef std::shared_ptr< bs_objinst_holder > sp_objinst;
@@ -43,26 +44,15 @@ private:
 	static unsigned int self_version();
 
 	BS_TYPE_INFO bs_ti_;
-	BS_TYPE_CREATION_FUN creation_fun_;
+	std::string type_name_; //!< string type name
+
+	//BS_TYPE_CREATION_FUN creation_fun_;
+	// map type of params tuple -> typeless creation function
+	std::unordered_map< std::type_index, std::pair< void*, void*> > creators_;
+
 	BS_TYPE_COPY_FUN copy_fun_;
 	BS_GET_TD_FUN parent_td_fun_;
-	std::string type_name_; //!< string type name
 	std::string description_; //!< Short description of type
-
-	//helper template to retrieve copy function
-	template< class T, int nocopy >
-	struct extract_copyfn {
-		static BS_TYPE_COPY_FUN go() {
-			return nullptr;
-		}
-	};
-
-	template< class T >
-	struct extract_copyfn< T, 0 > {
-		static BS_TYPE_COPY_FUN go() {
-			return T::bs_create_copy;
-		}
-	};
 
 	template< class T, class unused = void >
 	struct extract_tdfun {
@@ -89,51 +79,123 @@ private:
 		}
 	};
 
+	// helper to find out parameters pack tuple type
+	template< typename... Args >
+	using args_pack = std::tuple< std::decay_t< Args >... >;
+
+	// type of object construction callback
+	template< typename... Args >
+	using creator_callback = bs_type_ctor_result (*)(
+		void*, std::add_lvalue_reference_t< std::decay_t< Args > >...
+	);
+
 public:
 	// default constructor - type_descriptor points to nil
-	type_descriptor()
-		:
-		bs_ti_(nil_type_info()), creation_fun_(nullptr), copy_fun_(nullptr), parent_td_fun_(nullptr),
-		type_name_(BS_NIL_TYPE_TAG)
+	type_descriptor() :
+		bs_ti_(nil_type_info()), type_name_(BS_NIL_TYPE_TAG),
+		copy_fun_(nullptr), parent_td_fun_(nullptr)
 	{}
 
 	// Nil constructor for temporary tasks (searching etc)
-	type_descriptor(const std::string& stype)
-		:
-		bs_ti_(nil_type_info()), creation_fun_(nullptr), copy_fun_(nullptr), parent_td_fun_(nullptr),
-		type_name_(stype)
+	type_descriptor(const std::string& type_name) :
+		bs_ti_(nil_type_info()), type_name_(type_name), copy_fun_(nullptr), parent_td_fun_(nullptr)
 	{}
 
 	// standard constructor
 	type_descriptor(
-		const BS_TYPE_INFO& ti, const BS_TYPE_CREATION_FUN& cr_fn, const BS_TYPE_COPY_FUN& cp_fn,
-		const BS_GET_TD_FUN& parent_td_fn, const char* type_name, const char* description
+		const BS_TYPE_INFO& ti, const char* type_name, const BS_TYPE_COPY_FUN& cp_fn,
+		const BS_GET_TD_FUN& parent_td_fn, const char* description = ""
 	) :
-		bs_ti_(ti), creation_fun_(cr_fn), copy_fun_(cp_fn), parent_td_fun_(parent_td_fn),
-		type_name_(type_name), description_(description)
+		bs_ti_(ti), type_name_(type_name), copy_fun_(cp_fn), parent_td_fun_(parent_td_fn),
+		description_(description)
 	{}
 
 	// templated ctor for BlueSky types
-	//template< class T, class base = nil, int nocopy = 0 >
-	//type_descriptor(Loki::Type2Type< T > /* this_t */, Loki::Type2Type< base > /* base_t */,
-	//				Loki::Int2Type< nocopy > /* no_copy_fun */,
-	//				const char* type_name = nullptr, const char* description = nullptr)
-	//	:
-	//	bs_ti_(BS_GET_TI(T)), creation_fun_(T::bs_create_instance),
-	//	copy_fun_(extract_copyfn< T, nocopy >::go()),
-	//	parent_td_fun_(extract_tdfun< base >::go()),
-	//	type_name_(extract_typename< T >::go(type_name)), description_(description)
-	//{}
+	template< class T, class base = nil, class typename_t = std::nullptr_t >
+	type_descriptor(
+		typename_t type_name = nullptr, const char* description = nullptr,
+		bool add_std_construct = false, bool add_std_copy = false
+	) :
+		bs_ti_(BS_GET_TI(T)), type_name_(extract_typename< T >::go(type_name)),
+		copy_fun_(nullptr), parent_td_fun_(extract_tdfun< base >::go()),
+		description_(description)
+	{
+		if(add_std_construct)
+			add_constructor< T >();
+		if(add_std_copy)
+			set_copy_constructor< T >();
+	}
 
-	// templated ctor for BlueSky types
-	template< class T, class base = nil, int nocopy = 0, class typename_t = std::nullptr_t >
-	type_descriptor(typename_t type_name = nullptr, const char* description = nullptr)
-		:
-		bs_ti_(BS_GET_TI(T)), creation_fun_(T::bs_create_instance),
-		copy_fun_(extract_copyfn< T, nocopy >::go()),
-		parent_td_fun_(extract_tdfun< base >::go()),
-		type_name_(extract_typename< T >::go(type_name)), description_(description)
-	{}
+	/*-----------------------------------------------------------------
+	 * create new instance
+	 *----------------------------------------------------------------*/
+	// vanilla fucntion pointer as type constructor
+	template< typename... Args >
+	void add_constructor(bs_type_ctor_result (*f)(Args&&...)) {
+		creators_[typeid(args_pack< Args... >)] = std::make_pair(
+			reinterpret_cast< void* >(
+				[](void* ff, std::add_lvalue_reference_t< std::decay_t< Args > >... args) {
+					return (*static_cast< decltype(f) >(ff))(args...);
+				}
+			),
+			static_cast< void* >(f)
+		);
+	}
+
+	// std type construction
+	// enumerate constructor argument types
+	template< typename T, typename... Args >
+	void add_constructor() {
+		creators_[typeid(args_pack< Args... >)] = std::make_pair(
+			reinterpret_cast< void* >(
+				[](void*, std::add_lvalue_reference_t< std::decay_t< Args > >... args) {
+					return std::make_shared< T >(args...);
+				}
+			),
+			nullptr
+		);
+	}
+
+	// std type constructor
+	// deduce argument types from passed tuple
+	template< typename T, typename... Args >
+	void add_constructor(std::tuple< Args... >* = nullptr) {
+		add_constructor< T, Args... >();
+	}
+
+	// make new instance
+	template< typename... Args >
+	bs_type_ctor_result construct(Args&&... args) const {
+		auto creator = creators_.find(typeid(args_pack< Args... >));
+		if(creator != creators_.end()) {
+			auto callback = reinterpret_cast< creator_callback< Args...> >(creator->second.first);
+			return callback(creator->second.second, std::forward< Args >(args)...);
+		}
+		return nullptr;
+	}
+
+	/*-----------------------------------------------------------------
+	 * create copy of instance
+	 *----------------------------------------------------------------*/
+	// construct copy using copy constructor
+	template< typename T >
+	void add_copy_constructor() {
+		copy_fun_ = [](bs_type_copy_param src) {
+			return std::make_shared< T >(*src);
+		};
+	}
+
+	// construct copy using vanilla function
+	void add_copy_constructor(BS_TYPE_COPY_FUN f) {
+		copy_fun_ = f;
+	}
+
+	// make instance copy
+	bs_type_ctor_result clone(bs_type_copy_param src) {
+		if(copy_fun_)
+			return (*copy_fun_)(src);
+		return nullptr;
+	}
 
 	/// read access to base fields
 	BS_TYPE_INFO type() const {
