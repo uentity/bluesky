9299173ba6250d197d901328d38084cd7e75cc5b
uentity <unientity@gmail.com>
2017-03-21 09:34:23 +0500

    common: add bs.h that wraps complete BS API

---
+++ kernel/include/bs/bs.h
@@ -0,0 +1,25 @@
+/// @file
+/// @author uentity
+/// @date 21.03.2017
+/// @brief Include complete BS API
+/// @copyright
+/// This Source Code Form is subject to the terms of the Mozilla Public License,
+/// v. 2.0. If a copy of the MPL was not distributed with this file,
+/// You can obtain one at https://mozilla.org/MPL/2.0/
+
+#pragma once
+
+// kernel already contains a lot
+#include "kernel.h"
+#include "kernel_tools.h"
+// exceptions handling
+#include "exception.h"
+#include "throw_exception.h"
+// other things
+#include "misc.h"
+
+// for Python bindings include pybind11
+#if defined(BSPY_EXPORTING) || defined(BSPY_EXPORTING_PLUGIN)
+#include <pybind11/pybind11.h>
+#endif
+
