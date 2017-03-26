322f6446a87d13c1b77dec3a6e382c6e01966143
uentity <unientity@gmail.com>
2017-03-26 23:06:01 +0500

    python: add empty BS kernel Python subsystem

---
+++ kernel/src/python/py_main.cpp
@@ -0,0 +1,15 @@
+/// @file
+/// @author uentity
+/// @date 25.03.2017
+/// @brief Python subsystem of BS kernel
+/// @copyright
+/// This Source Code Form is subject to the terms of the Mozilla Public License,
+/// v. 2.0. If a copy of the MPL was not distributed with this file,
+/// You can obtain one at https://mozilla.org/MPL/2.0/
+
+#include <bs/bs.h>
+
+BLUE_SKY_INIT_PY_FUN {
+
+}
+
