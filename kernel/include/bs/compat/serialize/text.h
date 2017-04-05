76351515fb7b41485b28dbdee2aa5437fd65378b
uentity <unientity@gmail.com>
2017-04-05 12:30:38 +0500

    compat.serialize: ajust API of serialize to/from string functions
    Add 2nd optional template param to serialize_to_str() and
    serialize_from_str() and move corresponding casts inside, those, making
    *_indirect functions useless (remve them).
    
    Provide overloads for pure object pointers.

--- kernel/include/bs/compat/serialize/text.h
+++ kernel/include/bs/compat/serialize/text.h
@@ -23,51 +23,48 @@ namespace blue_sky {
 /*-----------------------------------------------------------------
  * helper functions that serialize any BS type to/from str
  *----------------------------------------------------------------*/
+// R is a type that is actually serialized
+// there should exist ststic conversion between T and R
 
-template< class T >
-BS_API_PLUGIN std::string serialize_to_str(const std::shared_ptr< T >& t) {
+template< class T, class R = T >
+std::string serialize_to_str(const std::shared_ptr< T >& t) {
 	typedef typename boost::archive::text_oarchive archive_t;
 	std::ostringstream os;
 	// ensure archive is destroyed before stream is closed
 	{
 		archive_t ar(os);
-		serialize_fix_data< archive_t >(ar) << t;
+		serialize_fix_data< archive_t >(ar) << std::static_pointer_cast< R >(t);
 	}
 	return os.str();
 }
 
-template< class T >
-BS_API_PLUGIN std::shared_ptr< T > serialize_from_str(const std::string& src) {
+// specializaton for pure BS type pointers (via shared_from_thos)
+template< class T, class R = T >
+std::string serialize_to_str(const T* src) {
+	return serialize_to_str(src->template bs_shared_this< const R >());
+}
+
+template< class T, class R = T >
+std::shared_ptr< T > serialize_from_str(const std::string& src) {
 	typedef typename boost::archive::text_iarchive archive_t;
 	std::istringstream is(src);
-	std::shared_ptr< T > t;
+	std::shared_ptr< R> t;
 	// ensure archive is destroyed before stream is closed
 	{
 		archive_t ar(is);
 		serialize_fix_data< archive_t >(ar) >> t;
 	}
-	return t;
+	return std::static_pointer_cast< T >(t);
 }
 
 template< class T >
-BS_API_PLUGIN std::shared_ptr< T > copy_via_serialize(const std::shared_ptr< T >& src) {
+std::shared_ptr< T > copy_via_serialize(const std::shared_ptr< T >& src) {
 	return serialize_from_str< T >(serialize_to_str< T >(src));
 }
 
-/*-----------------------------------------------------------------
- * indirect string serialization -- can be used with abstract interfaces
- *----------------------------------------------------------------*/
-// 2-nd param in both functions used only to pass type information
-// R is a temporary type used to actually invoke serialization sunctions inside
-
-template< class T, class R >
-BS_API_PLUGIN std::string serialize_to_str_indirect(const std::shared_ptr< T >& t) {
-	return serialize_to_str(std::static_pointer_cast< R, T >(t));
-}
-
-template< class T, class R >
-BS_API_PLUGIN std::shared_ptr< T > serialize_from_str_indirect(const std::string& src) {
-	return std::static_pointer_cast< T, R >(serialize_from_str< R >(src));
+template< class T >
+std::shared_ptr< T > copy_via_serialize(const T* src) {
+	return serialize_from_str< T >(serialize_to_str(src));
 }
 
 }  // eof blue_sky namespace
