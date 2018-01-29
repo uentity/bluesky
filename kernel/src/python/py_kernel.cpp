fa05a6ff57a3e74f584e6f7d98ee08835a4d3186
uentity <unientity@gmail.com>
2018-01-29 12:27:49 +0500

    python/any_array: implement basic binding for `any_array`
    Supports minimal set of dict-like operators.

--- kernel/src/python/py_kernel.cpp
+++ kernel/src/python/py_kernel.cpp
@@ -9,6 +9,8 @@
 
 #include <bs/bs.h>
 #include <bs/python/kernel.h>
+#include <bs/python/any.h>
+#include <boost/lexical_cast.hpp>
 
 NAMESPACE_BEGIN(blue_sky)
 NAMESPACE_BEGIN(python)
@@ -20,11 +22,64 @@ type_tuple test_type_tuple(const type_tuple& t) {
 	return t;
 }
 
+template<template<class> class array_traits, typename T>
+void set_any_array_value(
+	any_array<array_traits>& A, const typename any_array<array_traits>::key_type& k, T value
+) {
+	if(A.has_key(k))
+		A.template ss<T>(k) = std::move(value);
+	else
+		A.template insert_element(k, std::move(value));
 }
+
+template< template<class> class array_traits >
+void bind_any_array(py::module& m, const char* type_name) {
+	using any_array_t = any_array<array_traits>;
+	using key_type = typename any_array_t::key_type;
+
+	py::class_<any_array_t>(m, type_name)
+		.def(py::init<>())
+		.def("__bool__",
+			[](const any_array_t& A) -> bool { return !A.empty(); },
+			"Check whether the array is nonempty"
+		)
+		.def("__len__", (std::size_t (any_array_t::*)() const) &any_array_t::size)
+		.def_property_readonly("size", (std::size_t (any_array_t::*)() const) &any_array_t::size)
+		.def("__contains__", [](const any_array_t& A, const key_type& k) { return A.has_key(k); })
+		.def("__getitem__", [](const any_array_t& A, const key_type& k) {
+			using array_trait = typename any_array_t::trait;
+			auto pval = array_trait::find(A, k);
+			if(pval == A.end())
+				throw py::key_error("There's no element with key = " + boost::lexical_cast<std::string>(k));
+
+			return array_trait::iter2val(pval);
+		})
+		.def("__setitem__", [](any_array_t& A, const key_type& k, py::object value) {
+			if(py::isinstance<py::bool_>(value))
+				set_any_array_value(A, k, py::cast<bool>(value));
+			else if(py::isinstance<py::int_>(value))
+				set_any_array_value(A, k, py::cast<long>(value));
+			else if(py::isinstance<py::float_>(value))
+				set_any_array_value(A, k, py::cast<double>(value));
+			else if(py::isinstance<py::str>(value))
+				set_any_array_value(A, k, py::cast<std::string>(value));
+			else {
+				auto bsobj = py::cast<sp_obj>(value);
+				if(bsobj) set_any_array_value(A, k, std::move(bsobj));
+				else throw py::value_error("Only primitive types and BS objects can be stored in any_array");
+			}
+		})
+	;
+}
+
+} // eof hodden namespace
 
 void py_bind_kernel(py::module& m) {
 
 	m.def("test_type_tuple", &test_type_tuple);
+
+	bind_any_array<str_any_traits>(m, "str_any_array");
+	bind_any_array<idx_any_traits>(m, "idx_any_array");
 
 	py::class_<kernel, std::unique_ptr<kernel, py::nodelete>>(m, "kernel")
 		.def("load_plugin", &kernel::load_plugin,
@@ -51,8 +106,8 @@ void py_bind_kernel(py::module& m) {
 		.def("register_instance", (int (kernel::*)(const sp_obj&)) &kernel::register_instance)
 		.def("free_instance", (int (kernel::*)(const sp_obj&)) &kernel::free_instance)
 		.def("instances", (kernel::instances_enum (kernel::*)(const BS_TYPE_INFO&) const) &kernel::instances)
-		.def("pert_str_any_array", &kernel::pert_str_any_array)
-		.def("pert_idx_any_array", &kernel::pert_idx_any_array)
+		.def("pert_str_any_array", &kernel::pert_str_any_array, py::return_value_policy::reference_internal)
+		.def("pert_idx_any_array", &kernel::pert_idx_any_array, py::return_value_policy::reference_internal)
 
 		.def("clone_object", [](const kernel& k, bs_type_copy_param src) -> sp_obj {
 			return k.clone_object(src);
