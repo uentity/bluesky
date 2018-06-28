53088a29175355de6795227cfd711aeb283090cd
uentity <unientity@gmail.com>
2018-06-28 15:28:05 +0500

    serial/atomizer: add support for non-versioned serialization functions

--- kernel/include/bs/serialize/atomizer.h
+++ kernel/include/bs/serialize/atomizer.h
@@ -56,6 +56,9 @@ public:
  *  that forwards call to `blue_sky::atomizer` (which has access to private members)
  *-----------------------------------------------------------------------------*/
 namespace cereal {
+///////////////////////////////////////////////////////////////////////////////
+//  forward versioned functions
+//
 // cereal::save -> blue_sky::atomizer::save::go
 template<
 	typename Archive, typename T, typename = decltype( ::blue_sky::atomizer::save<T>::go(
@@ -107,6 +110,61 @@ template<
 >
 auto load_minimal(Archive const& ar, T& t, V const& v, std::uint32_t const version) -> void {
 	::blue_sky::atomizer::load_minimal<T>::go(ar, t, v, version);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//  forward non-versioned functions
+//
+// cereal::save -> blue_sky::atomizer::save::go
+template<
+	typename Archive, typename T, typename = decltype( ::blue_sky::atomizer::save<T>::go(
+		std::declval<Archive&>(), std::declval<T const&>()
+	))
+>
+auto save(Archive& ar, T const& t) -> void {
+	::blue_sky::atomizer::save<T>::go(ar, t);
+}
+
+// cereal::load -> blue_sky::atomizer::load::go
+template<
+	typename Archive, typename T, typename = decltype( ::blue_sky::atomizer::load<T>::go(
+		std::declval<Archive&>(), std::declval<T&>()
+	))
+>
+auto load(Archive& ar, T& t) -> void {
+	::blue_sky::atomizer::load<T>::go(ar, t);
+}
+
+// cereal::serialize -> blue_sky::atomizer::serialize::go
+template<
+	typename Archive, typename T, typename = decltype( ::blue_sky::atomizer::serialize<T>::go(
+		std::declval<Archive&>(), std::declval<T&>()
+	))
+>
+auto serialize(Archive& ar, T& t) -> void {
+	::blue_sky::atomizer::serialize<T>::go(ar, t);
+}
+
+// cereal::save_minimal -> blue_sky::atomizer::save_minimal::go
+template<
+	typename Archive, typename T, typename = decltype( ::blue_sky::atomizer::save_minimal<T>::go(
+		std::declval<Archive const&>(), std::declval<T const&>()
+	))
+>
+auto save_minimal(Archive const& ar, T const& t) {
+	return ::blue_sky::atomizer::save_minimal<T>::go(ar, t);
+}
+
+// cereal::load_minimal -> blue_sky::atomizer::load_minimal::go
+template<
+	typename Archive, typename T, typename V, typename = decltype(
+		::blue_sky::atomizer::load_minimal<T>::go(
+			std::declval<Archive const&>(), std::declval<T&>(), std::declval<V const&>()
+		)
+	)
+>
+auto load_minimal(Archive const& ar, T& t, V const& v) -> void {
+	::blue_sky::atomizer::load_minimal<T>::go(ar, t, v);
 }
 
 } // eof cereal namespace
