2968cf520b62171dfc25d0b6d134599035881fa0
uentity <unientity@gmail.com>
2018-08-17 11:42:50 +0500

    async: add mixin that implements pattern of async API call via `scoped_actor`

---
+++ kernel/include/bs/detail/async_api_mixin.h
@@ -0,0 +1,42 @@
+/// @file
+/// @author uentity
+/// @date 16.08.2018
+/// @brief Pattern of async API call using CAF actors
+/// @copyright
+/// This Source Code Form is subject to the terms of the Mozilla Public License,
+/// v. 2.0. If a copy of the MPL was not distributed with this file,
+/// You can obtain one at https://mozilla.org/MPL/2.0/
+#pragma once
+
+#include "../kernel.h"
+#include <caf/scoped_actor.hpp>
+
+namespace blue_sky { namespace detail {
+
+template<class Derived>
+class async_api_mixin {
+public:
+	explicit async_api_mixin() : sender_(BS_KERNEL.actor_system(), true) {}
+
+	// link sender with target actor, so they die together
+	auto init_sender() const -> void {
+		sender_->link_to(derived().actor());
+	}
+
+	// pass any message to target actor
+	template<typename... Args>
+	auto send(Args&&... args) const {
+		return sender_->send(derived().actor(), std::forward<Args>(args)...);
+	}
+
+	auto sender() const -> const caf::scoped_actor& {
+		return sender_;
+	}
+
+private:
+	caf::scoped_actor sender_;
+	inline auto derived() const -> const Derived& { return static_cast<const Derived&>(*this); }
+};
+	
+}} /* namespace blue_sky::detail */
+
