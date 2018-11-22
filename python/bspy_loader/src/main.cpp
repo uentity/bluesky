6c006a8e6d1305a638e1145f8600b002be11a29e
uentity <unientity@gmail.com>
2018-11-22 10:03:33 +0500

    bspy_loader: update according to latest pybind11, update license

--- python/bspy_loader/src/main.cpp
+++ python/bspy_loader/src/main.cpp
@@ -1,17 +1,12 @@
-// This file is part of BlueSky
-// 
-// BlueSky is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public License
-// as published by the Free Software Foundation; either version 3
-// of the License, or (at your option) any later version.
-// 
-// BlueSky is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-// 
-// You should have received a copy of the GNU General Public License
-// along with BlueSky; if not, see <http://www.gnu.org/licenses/>.
+/// @file
+/// @author uentity
+/// @date 22.11.2018
+/// @brief Python loader for blue-sky-re kernel
+/// @copyright
+/// This Source Code Form is subject to the terms of the Mozilla Public License,
+/// v. 2.0. If a copy of the MPL was not distributed with this file,
+/// You can obtain one at https://mozilla.org/MPL/2.0/
+
 
 #include <bs/common.h>
 #include <bs/kernel.h>
@@ -24,9 +19,8 @@
 #include <boost/preprocessor/stringize.hpp>
 
 #include <string>
-#include <exception>
 
-//! define TARGET_NAME in compiler equal to output loader's name without extension
+/// TARGET_NAME must match built library name
 #ifndef TARGET_NAME
 #define TARGET_NAME bs
 #endif
@@ -38,23 +32,18 @@ using namespace blue_sky;
 using namespace std;
 namespace py = pybind11;
 
-PYBIND11_PLUGIN(TARGET_NAME) {
+PYBIND11_MODULE(TARGET_NAME, m) {
 	// search for BlueSky's kernel plugin descriptor
 	BS_GET_PLUGIN_DESCRIPTOR get_pd_fn;
 	if(detail::lib_descriptor::load_sym_glob("bs_get_plugin_descriptor", get_pd_fn) != 0 || !get_pd_fn) {
-		BSERROR << log::E("BlueSky kernel descriptor wasn't found or invalid!") << log::end;
-		return nullptr;
-		//throw std::runtime_error("BlueSky kernel descriptor wasn't found");
+		throw error("BlueSky kernel descriptor wasn't found or invalid!");
 	}
 	plugin_descriptor* kernel_pd = get_pd_fn();
-	// correct global namespace to match TARGET_NAME (otherwise Python throws an error)
+	// Python namespace must match TARGET_NAME (otherwise Python throws an error)
 	kernel_pd->py_namespace = S_TARGET_NAME;
-
-	auto m = py::module(S_TARGET_NAME, kernel_pd->description.c_str());
+	m.doc() = kernel_pd->description;
 
 	//load plugins with Python subsystem
 	give_kernel::Instance().load_plugins(&m);
-
-	return m.ptr();
 }
 
