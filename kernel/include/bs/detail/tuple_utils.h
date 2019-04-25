04f00af8f35d2de70db54d39c889c12fb29696f4
uentity <unientity@gmail.com>
2019-04-25 19:05:35 +0500

    detail: forgot perfect forwarding in visit_tuple()

--- kernel/include/bs/detail/tuple_utils.h
+++ kernel/include/bs/detail/tuple_utils.h
@@ -120,7 +120,7 @@ constexpr decltype(auto) grow_tuple(
 template<typename F, typename T>
 constexpr auto visit_tuple(F&& f, T&& tup) -> std::enable_if_t<is_tuple<T>> {
 	std::apply([f = std::forward<F>(f)](auto&&... tup_elem) {
-		(f(tup_elem), ...);
+		( f(std::forward<decltype(tup_elem)>(tup_elem)), ... );
 	}, std::forward<T>(tup));
 }
 
