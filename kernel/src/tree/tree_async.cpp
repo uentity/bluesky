746edf49dd46551a80b03166d1e4e2e1fc34b01b
uentity <unientity@gmail.com>
2019-04-30 18:49:52 +0500

    async/deref_path: eliminate static variable that keeps strong actor ref
    Refactor.
    1. Use `anon_async_api_mixin`.
    2. Implement scheme:
      a) spawn actor with `lazy_init` flag & store strong ref in local variable on stack;
      b) send message to it;
      c) forget actor and return.
    
    Actor is spawned from `anon_async_api_mixin` with leave actor alive flag
    set. Because of `lazy_init` flag actor initialization isn't performed
    until message comes, and that message is immediately sent.
    CAF should init actor & invoke message processing at least once. Then,
    no strong ref to actor is left and it should be auto-destructed by CAF
    runtime.

--- kernel/src/tree/tree_async.cpp
+++ kernel/src/tree/tree_async.cpp
@@ -22,37 +22,37 @@ CAF_ALLOW_UNSAFE_MESSAGE_TYPE(blue_sky::tree::deref_process_f)
 using walk_down_ft = decltype( blue_sky::tree::detail::gen_walk_down_tree() );
 CAF_ALLOW_UNSAFE_MESSAGE_TYPE(walk_down_ft)
 
-NAMESPACE_BEGIN(blue_sky::tree) NAMESPACE_BEGIN(detail)
+NAMESPACE_BEGIN(blue_sky::tree)
 
 /*-----------------------------------------------------------------------------
  *  implementation details
  *-----------------------------------------------------------------------------*/
 NAMESPACE_BEGIN()
+
+template <typename level_process_f>
+using deref_actor_t = caf::typed_actor<
+	caf::reacts_to<std::string, sp_link, level_process_f, deref_process_f, bool>
+>;
+
 template<typename level_process_f>
-struct deref_actor : blue_sky::detail::async_api_mixin<deref_actor<level_process_f>> {
-	using base_t = blue_sky::detail::async_api_mixin<deref_actor<level_process_f>>;
-	using deref_actor_t = caf::typed_actor<
-		caf::reacts_to<std::string, sp_link, level_process_f, deref_process_f, bool>
-	>;
+struct deref_actor : blue_sky::detail::anon_async_api_mixin< deref_actor_t<level_process_f> > {
+	using actor_t = deref_actor_t<level_process_f>;
+	using base_t = blue_sky::detail::anon_async_api_mixin<actor_t>;
 
-	deref_actor_t actor_;
-	auto actor() const -> const deref_actor_t& { return actor_; }
-
-	deref_actor()
-		: actor_(blue_sky::kernel::config::actor_system().spawn(behaviour))
-	{
-		base_t::init_sender();
+	// lazy init actor only when message arrives
+	// and don't auto-kill actor when this dies
+	deref_actor() : base_t(false) {
+		base_t::template spawn<caf::spawn_options::lazy_init_flag>(async_behavior);
 	}
 
-private:
-	static auto behaviour(typename deref_actor_t::pointer self) -> typename deref_actor_t::behavior_type {
+	static auto async_behavior(typename actor_t::pointer self) -> typename actor_t::behavior_type {
 		return {
 			[](
 				const std::string& path, sp_link lnk,
 				const level_process_f& lp, const deref_process_f& f,
 				bool follow_lazy_links
 			) {
-				f(deref_path_impl(path, std::move(lnk), nullptr, follow_lazy_links, lp));
+				f(detail::deref_path_impl(path, std::move(lnk), nullptr, follow_lazy_links, lp));
 			}
 		};
 	}
@@ -60,26 +60,6 @@ private:
 };
 
 NAMESPACE_END()
-
-template<typename DerefProcessF>
-auto deref_path_async(
-	std::string path, sp_link lnk, walk_down_ft&& lp, DerefProcessF&& dp,
-	bool follow_lazy_links, bool high_priority
-) -> void {
-	// initialize actor only once
-	static deref_actor<walk_down_ft> actor;
-
-	// sanity
-	if(!lnk) dp(nullptr);
-	// send message
-	actor.send(
-		high_priority ? caf::message_priority::high : caf::message_priority::normal,
-		std::move(path), std::move(lnk), std::move(lp), std::move(dp),
-		follow_lazy_links
-	);
-}
-
-NAMESPACE_END(detail)
 
 /*-----------------------------------------------------------------------------
  *  async tree fucntions
@@ -89,11 +69,18 @@ auto deref_path(
 	deref_process_f f, std::string path, sp_link start, node::Key path_unit,
 	bool follow_lazy_links, bool high_priority
 ) -> void {
-	detail::deref_path_async<deref_process_f>(
-		std::move(path), std::move(start),
-		detail::gen_walk_down_tree(path_unit), std::move(f),
-		follow_lazy_links, high_priority
+	// create local temp actor
+	deref_actor<walk_down_ft> actor;
+
+	// send message
+	actor.send(
+		high_priority ? caf::message_priority::high : caf::message_priority::normal,
+		std::move(path), std::move(start), detail::gen_walk_down_tree(path_unit), std::move(f),
+		follow_lazy_links
 	);
+	// and forget about actor
+	// 1. message should arrive, then actor initialization is performed, then it gets executed
+	// 2. and after that actor itself dies because no more strong references to it exist
 }
 
 NAMESPACE_END(blue_sky::tree)
