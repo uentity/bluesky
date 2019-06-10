dff15442cf1d24c73f18b0b7e9d47b257666993a
uentity <unientity@gmail.com>
2019-06-10 18:32:30 +0500

    meta: add `is_map` trait, move `is_container` to 'meta' namespace

--- kernel/include/bs/detail/is_container.h
+++ kernel/include/bs/detail/is_container.h
@@ -12,30 +12,38 @@
 #include <type_traits>
 #include <iterator>
 
-namespace blue_sky {
-namespace detail {
+namespace blue_sky::meta {
 
-template<typename... Ts>
-struct is_container_helper {};
+/// helper type for SFINAE
+template<typename...> using void_t  = void;
 
-} // detail
-
-/// Trait to test if given type is container-like
+///////////////////////////////////////////////////////////////////////////////
+//  Test if given type is container-like (can be iterated)
+//
 template<typename T, typename = void>
 struct is_container : std::false_type {};
 
 template<typename T>
-struct is_container<
-	T,
-	std::conditional_t<
-		false,
-		detail::is_container_helper<
-			decltype(std::begin(std::declval<std::decay_t<T>>())),
-			decltype(std::end(std::declval<std::decay_t<T>>()))
-		>,
-		void
-	>
-> : public std::true_type {};
+struct is_container<T, void_t<
+	decltype(std::begin(std::declval<std::decay_t<T>>())),
+	decltype(std::end(std::declval<std::decay_t<T>>()))
+>> : public std::true_type {};
+
+template<typename T> inline constexpr auto is_container_v = is_container<T>::value;
+
+///////////////////////////////////////////////////////////////////////////////
+//  Test if given type is map-like (container that has `mapped_type`)
+//
+template<typename T, typename = void>
+struct is_map : std::false_type { using mapped_type = void; };
+
+template<typename T>
+struct is_map<T, std::enable_if_t<is_container_v<T>, void_t<typename T::mapped_type>>> : std::true_type {
+	using mapped_type = typename T::mapped_type;
+};
+
+template<typename T> using mapped_type = typename is_map<T>::mapped_type;
+template<typename T> inline constexpr auto is_map_v = is_map<T>::value;
 
 } // blue_sky
 
