a2b8b18895d50ed532146983b3fcb4ffbcc38f63
uentity <unientity@gmail.com>
2019-07-14 21:01:07 +0500

    tests: add basic link events handlers test

--- tests/test_tree.cpp
+++ tests/test_tree.cpp
@@ -11,22 +11,33 @@
 
 #include "test_objects.h"
 #include "test_serialization.h"
+
+#include <bs/log.h>
+#include <bs/propdict.h>
 #include <bs/kernel/kernel.h>
 #include <bs/kernel/tools.h>
-#include <bs/log.h>
 #include <bs/tree/tree.h>
 
 #include <bs/serialize/base_types.h>
 #include <bs/serialize/array.h>
 #include <bs/serialize/tree.h>
 
+#include <boost/uuid/uuid_io.hpp>
 #include <boost/test/unit_test.hpp>
-#include <iostream>
 #include <caf/scoped_actor.hpp>
+
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <atomic>
 
 using namespace blue_sky;
 using namespace blue_sky::log;
 using namespace blue_sky::tree;
+using namespace std::chrono_literals;
+
+using Req = link::Req;
+using ReqStatus = link::ReqStatus;
 
 namespace {
 
@@ -53,7 +64,7 @@ BOOST_AUTO_TEST_CASE(test_tree) {
 	// person link
 	auto L = std::make_shared<hard_link>("person link", std::move(P));
 	BOOST_TEST(L);
-	auto L1 = test_json(L);
+	auto L1 = test_json(L, false);
 	BOOST_TEST(L->name() == L1->name());
 
 	// create root link and node
@@ -114,12 +125,47 @@ BOOST_AUTO_TEST_CASE(test_tree) {
 		lnk->obj_type_id() << ' ' << (void*)lnk->data().get() << std::endl;
 	}, "hard_Citizen_0", hN, node::Key::Name);
 
-	// fusion link
-	//{
-	//	auto fl = std::make_shared<fusion_link>(
-	//		"fuse1", nullptr, std::make_shared<fusion_client>()
-	//	);
-	//	//fl->test();
-	//}
+	// test link events
+	std::atomic<int> rename_cnt = 0;
+	auto h_rename = L->subscribe(link::Event::Renamed, [&](sp_link who, prop::propdict what) -> void {
+		using namespace blue_sky::prop;
+		++rename_cnt;
+		bsout() << "=> {}: renamed '{}' -> '{}'" << to_string(who->id())
+			<< get<std::string>(what, "prev_name") << get<std::string>(what, "new_name") << bs_end;
+	});
+
+	std::atomic<int> status_cnt = 0;
+	auto h_status = L->subscribe(link::Event::StatusChanged, [&](sp_link who, prop::propdict what) {
+		using namespace blue_sky::prop;
+		++status_cnt;
+		bsout() << "=> {}: status {}: {} -> {}" << to_string(who->id()) <<
+			get<integer>(what, "request") << get<integer>(what, "prev_status") << 
+			get<integer>(what, "new_status") << bs_end;
+	});
+
+	bsout() << "\n===========================\n" << bs_end;
+	// try rename
+	L->rename("Test rename event");
+	// try change status
+	L->rs_reset(Req::Data, ReqStatus::Error);
+	L->rs_reset(Req::Data, ReqStatus::OK);
+	//std::this_thread::sleep_for(10ms);
+
+	// disconnect renamer
+	//L->unsubscribe(h_rename);
+	//std::this_thread::sleep_for(10ms);
+	L->rename("Can't see me");
+	
+	// disconnect status
+	L->rs_reset(Req::Data, ReqStatus::Error);
+	//std::this_thread::sleep_for(10ms);
+	//L->unsubscribe(h_status);
+	// nobody will know abut that
+	//std::this_thread::sleep_for(10ms);
+	L->rs_reset(Req::Data, ReqStatus::OK);
+
+	std::this_thread::sleep_for(500ms);
+	std::cout << "### rename calls: " << rename_cnt << std::endl;
+	std::cout << "### status calls: " << status_cnt << std::endl;
 }
 
