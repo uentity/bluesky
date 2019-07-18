159b650ad7b192d4cc82a961e3dc351176348adc
uentity <unientity@gmail.com>
2019-07-18 14:59:24 +0500

    tree/link: fix data_changed signal broadcasting

--- kernel/src/tree/link.cpp
+++ kernel/src/tree/link.cpp
@@ -95,7 +95,7 @@ auto link::rs_reset_if_neq(Req request, ReqStatus self, ReqStatus new_rs) const 
 
 auto link::rs_data_changed() const -> void {
 	// unconditionally sends Data = OK, ack
-	pimpl_->send(pimpl_, a_lnk_status(), a_ack(), Req::Data, ReqStatus::OK, req_status(Req::Data));
+	pimpl_->send(pimpl_->self_grp, a_lnk_status(), a_ack(), Req::Data, ReqStatus::OK, req_status(Req::Data));
 }
 
 
