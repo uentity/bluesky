f41df74fd9159a166655ac43e1231d66eb0fe53a
uentity <unientity@gmail.com>
2020-03-24 19:25:55 +0500

    tree/context: implementation fixes & refactoring
    1. Fix conversion from root node link and path into tag.
       Verify tag for intermediate root links during conversion.
    2. `impl::push()` returns pointer, because it's what being used.
    3. `pop()` removes whole subtree of given path, because it's also
       incorrect.
    4. Spot places where we can auto-remove incorrect tags, but don't do it,
       because clients (Qt model) relies on tags existence.

--- kernel/src/tree/context.cpp
+++ kernel/src/tree/context.cpp
@@ -7,7 +7,6 @@
 /// v. 2.0. If a copy of the MPL was not distributed with this file,
 /// You can obtain one at https://mozilla.org/MPL/2.0/
 
-#include <algorithm>
 #include <bs/log.h>
 #include <bs/tree/context.h>
 #include <bs/kernel/radio.h>
@@ -20,8 +19,10 @@
 
 #include <unordered_map>
 #include <unordered_set>
+#include <algorithm>
 
 NAMESPACE_BEGIN(blue_sky::tree)
+[[maybe_unused]] const auto uuid_from_str = boost::uuids::string_generator{};
 
 auto to_string(const lids_v& path, bool as_absolute) -> std::string {
 	auto res = std::vector<std::string>(as_absolute ? path.size() + 1 : path.size());
@@ -35,8 +36,6 @@ auto to_string(const lids_v& path, bool as_absolute) -> std::string {
 
 // convert string path to vector of lids
 static auto to_lids_v(const std::string& path) -> lids_v {
-	[[maybe_unused]] static const auto uuid_from_str = boost::uuids::string_generator{};
-
 	auto path_split = std::vector< std::pair<std::string::const_iterator, std::string::const_iterator> >{};
 	boost::split(path_split, path, boost::is_any_of("/"));
 	if(path_split.empty()) return {};
@@ -51,6 +50,22 @@ static auto to_lids_v(const std::string& path) -> lids_v {
 	return res;
 }
 
+template<typename Path>
+static auto concat(Path&& lhs, lid_type rhs) {
+	// detect that we can move from lhs
+	if constexpr(!std::is_reference_v<Path> && !std::is_const_v<Path>) {
+		lhs.push_back(std::move(rhs));
+		return lhs;
+	}
+	else {
+		using path_t = std::decay_t<Path>;
+		auto leaf_path = path_t(lhs.size() + 1);
+		std::copy(lhs.begin(), lhs.end(), leaf_path.begin());
+		leaf_path.back() = std::move(rhs);
+		return leaf_path;
+	}
+}
+
 // enters data node only if allowed to (don't auto-expand lazy links)
 static auto data_node(const link& L) -> sp_node {
 	return L.data_node(unsafe);
@@ -60,8 +75,7 @@ static auto data_node(const link& L) -> sp_node {
 template<typename PathIterator>
 static auto deref_path(PathIterator from, PathIterator to, const link& root) -> link {
 	auto res = link{};
-	auto level = root;
-	for(; from != to; ++from) {
+	for(auto level = root; from != to; ++from) {
 		if(auto N = data_node(level)) {
 			if(( res = N->find(*from) ))
 				level = res;
@@ -70,6 +84,13 @@ static auto deref_path(PathIterator from, PathIterator to, const link& root) -> 
 		}
 	}
 	return res;
+}
+
+// sentinel
+inline constexpr auto none = context::item_index{{}, -1};
+
+inline static auto is_valid(const context::item_index& i) -> bool {
+	return !(i == none);
 }
 
 /*-----------------------------------------------------------------------------
@@ -140,36 +161,30 @@ struct BS_HIDDEN_API context::impl {
 		verify();
 	}
 
-	auto push(path_t path, const link& item = {}) -> item_tag& {
+	// [NOTE] always returns valid pointer
+	auto push(path_t path, const link& item = {}) -> item_tag const* {
 		auto [pos, is_inserted] = idata_.try_emplace(std::move(path), item);
 		// update existing tag if valid link passed in
 		if(!is_inserted && item)
 			pos->second = item;
 		// DEBUG
 		//dump();
-		return *pos;
+		return &*pos;
 	}
 
-	decltype(auto) push(const path_t& base, lid_type leaf, const link& item = {}) {
-		auto leaf_path = path_t(base.size() + 1);
-		std::copy(base.begin(), base.end(), leaf_path.begin());
-		leaf_path.back() = std::move(leaf);
-		return push(std::move(leaf_path), item);
+	template<typename Path>
+	auto push(Path&& base, lid_type leaf, const link& item = {}) {
+		return push( concat(std::forward<Path>(base), std::move(leaf)), item );
 	}
 
-	auto pop(const path_t& path) -> bool {
-		if(auto pos = idata_.find(path); pos != idata_.end()) {
-			idata_.erase(pos);
-			return true;
-		}
-		return false;
-	}
-
-	template<typename Item>
-	auto pop_item(const Item& item) -> std::size_t {
+	// erases all subpath under given path
+	// because if parent is invalid -> all child indexes are also invalid
+	auto pop(const path_t& path, std::optional<idata_t::iterator> hint = {}) -> std::size_t {
+		const auto n = path.size();
 		std::size_t cnt = 0;
-		for(auto pos = idata_.begin(); pos != idata_.end();) {
-			if(pos->second == item) {
+		for(auto pos = hint ? *hint : idata_.begin(); pos != idata_.end();) {
+			const auto& pos_path = pos->first;
+			if(pos_path.size() >= n && std::equal(path.begin(), path.end(), pos_path.begin())) {
 				idata_.erase(pos++);
 				++cnt;
 			}
@@ -179,11 +194,88 @@ struct BS_HIDDEN_API context::impl {
 		return cnt;
 	}
 
-	// searh idata for given link (no midifications are made)
-	auto find(const link& what) const -> existing_tag {
+	template<typename Item>
+	auto pop_item(const Item& item) -> std::size_t {
+		std::size_t cnt = 0;
+		while(true) {
+			if(auto pos = std::find_if(
+				idata_.begin(), idata_.end(), [&](auto& i) { return i.second == item; }
+			); pos != idata_.end())
+				cnt += pop(pos->first, pos);
+			else
+				break;
+		}
+		return cnt;
+	}
+
+	// find tag by path - returns unique element
+	auto find(const std::string& path) const -> existing_tag {
+		if(path.empty() || path == "/") return {};
+		// conversion from string -> lids vector can throw if string is non-valid path
+		try {
+			if(auto r = idata_.find(to_lids_v(path)); r != idata_.end())
+				return &*r;
+		} catch(...) {}
+		return {};
+	}
+
+	// find tag by link - can return multiple tags for same link, including invalid ones
+	auto find(const link& what) const -> std::vector<existing_tag> {
+		auto res = std::vector<existing_tag>{};
 		for(auto pos = idata_.begin(), end = idata_.end(); pos != end; ++pos) {
 			if(pos->second == what)
-				return &*pos;
+				res.push_back(&*pos);
+		}
+		return res;
+	}
+
+	// verify tag by comparing with given link
+	// on success return valid item index
+	auto verify_item_index(existing_tag L_tag, const link& L) -> item_index {
+		const auto parent_node = L.owner();
+		if(!parent_node) return none;
+
+		const auto& [L_path, L_ptr] = **L_tag;
+		if(L_ptr == L) {
+			// check that cached parent matches link's parent
+			auto L_row = node::existing_index{};
+			if(L_path.size() == 1 && parent_node == root_)
+				L_row = parent_node->index(L.id());
+			else {
+				auto [parent, _] = make(L_tag);
+				if(parent) {
+					const auto& [_, parent_ptr] = **parent;
+					if(data_node(parent_ptr.lock()) == parent_node)
+						L_row = parent_node->index(L.id());
+				}
+			}
+			// if valid row is found - return, otherwise remove incorrect tag
+			if(L_row) return { std::move(L_tag), *L_row };
+			// [NOTE] disabled, because clients may rely on invalid entries
+			//else
+			//	pop(L_path);
+		}
+		return none;
+	}
+
+	// simpler and quicker version of above when item row inside parent isn't needed
+	auto verify_tag(existing_tag L_tag, const link& L) -> existing_tag {
+		const auto parent_node = L.owner();
+		if(!parent_node) return {};
+
+		const auto& [L_path, L_ptr] = **L_tag;
+		if(L_ptr == L) {
+			// check that cached parent matches link's parent
+			if(L_path.size() == 1 && parent_node == root_)
+				return L_tag;
+			else {
+				auto [parent, _] = make(L_tag);
+				if(parent) {
+					const auto& [_, parent_ptr] = **parent;
+					if(data_node(parent_ptr.lock()) == parent_node)
+						return L_tag;
+				}
+			}
 		}
 		return {};
 	}
@@ -191,75 +283,86 @@ struct BS_HIDDEN_API context::impl {
 	auto make(const std::string& path, bool nonexact_match = false) -> item_index {
 		//bsout() << "*** index_from_path()" << bs_end;
 		auto level = root_;
-		auto res = item_index{{}, -1};
+		auto res = none;
 
-		auto cur_subpath = std::string{};
+		auto cur_subpath = lids_v{};
 		auto push_subpath = [&](const std::string& next_lid, const sp_node& cur_level) {
 			if(auto item = level->find(next_lid, Key::ID)) {
-				cur_subpath += '/';
-				cur_subpath += next_lid;
-				res = { &push(to_lids_v(cur_subpath), item), level->index(item.id()).value_or(-1) };
-				return item;
+				if(auto item_row = level->index(item.id())) {
+					cur_subpath.push_back(uuid_from_str(next_lid));
+					res = { push(cur_subpath, item), *item_row };
+					return item;
+				}
 			}
 			return link{};
 		};
 
 		// walk down tree
-		detail::deref_path_impl(path, root_lnk_, root_, false, std::move(push_subpath));
-		return nonexact_match || path == cur_subpath ? res : item_index{{}, -1};
+		detail::deref_path_impl(path, root_lnk_, {}, false, std::move(push_subpath));
+		return nonexact_match || path == to_string(cur_subpath) ? res : none;
 	}
 
 	auto make(const link& L, std::string path_hint = "/") -> item_index {
 		//bsout() << "*** index_from_link()" << bs_end;
+		// link is expected to be inside root's subtree
+		const auto parent_node = L.owner();
+		if(!parent_node) return none;
+
 		// make path hint start relative to current model root
-		// [NOTE] including leading slash!
 		auto rootp = abspath(root_lnk_, Key::ID);
 		// if path_hint.startswith(rootp)
 		if(path_hint.size() >= rootp.size() && std::equal(rootp.begin(), rootp.end(), path_hint.begin()))
 			path_hint = path_hint.substr(rootp.size());
 
 		// get search starting point & ensure it's cached in idata
-		auto start_tag = make(path_hint, true).first;
+		auto start_index = make(path_hint, true);
+		auto start_tag = start_index.first;
 		auto start_link = start_tag ? (**start_tag).second.lock() : root_lnk_;
+		if(start_link == L) return start_index;
 
 		// walk down from start node and do step-by-step search for link
 		// all intermediate paths are added to `idata`
-		auto res = item_index{{}, -1};
-		auto develop_link = [&, Lid = L.id()](link R, std::list<link>& nodes, std::vector<link>& objs)
+		auto res = none;
+		auto develop_link = [&](link R, std::list<link>& nodes, std::vector<link>& objs)
 		mutable {
 			// get current root path
 			auto R_path = path_t{};
-			if(auto R_tag = find(R))
-				R_path = (**R_tag).first;
-			else if(R != root_lnk_) {
+			if(R == start_link && R != root_lnk_)
+				R_path = (**start_tag).first;
+			else {
+				for(auto r : find(R))
+					if(auto rtag = verify_tag(std::move(r), R)) {
+						R_path = (**rtag).first;
+						break;
+					}
+			}
+			// if root path wasn't found - return
+			if(R != root_lnk_ && R_path.empty()) {
 				nodes.clear();
 				return;
 			}
 
 			// leaf checker
-			bool found = false;
-			const auto check_link = [&](auto& item) {
+			const auto check_link = [&, Lid = L.id()](auto& item) {
 				if(item.id() == Lid) {
 					if(auto Rnode = data_node(R)) {
-						if(auto row = Rnode->index(Lid)) {
-							res = { &push(R_path, std::move(Lid), item), *row };
-							found = true;
-						}
+						if(auto row = Rnode->index(Lid))
+							res = { push(R_path, std::move(Lid), item), *row };
 					}
 				}
-				return found;
+				return is_valid(res);
 			};
 
-			// check each leaf (both nodes & links)
-			for(const auto& N : nodes) { if(check_link(N)) break; }
-			if(!found) {
-				for(const auto& O : objs) { if(check_link(O)) break; }
-			}
+			// first check object leafs
+			for(const auto& O : objs) { if(check_link(O)) break; }
+			// next check each nodes leafs
+			if(!is_valid(res))
+				for(const auto& N : nodes) { if(check_link(N)) break; }
 			// if we found a link, stop further iterations
-			if(found) nodes.clear();
+			if(is_valid(res)) nodes.clear();
 		};
 
-		walk(start_link, develop_link);
+		walk(start_link, std::move(develop_link));
 		return res;
 	}
 
@@ -275,58 +378,70 @@ struct BS_HIDDEN_API context::impl {
 		// obtain child link
 		auto child_link = [&] {
 			if(auto par_sz = (std::int64_t)parent_node->size(); row < par_sz) {
-				// sometimes we can get negative row, it just means last element
-				if(row < 0) row = par_sz - 1;
+				// support negative indexing from node end
+				if(row < 0) row += par_sz;
 				if(row >= 0) return parent_node->find((std::size_t)row);
 			}
 			return link{};
 		}();
 
 		if(child_link)
-			return &push(parent_node != root_ ? (**parent).first : lids_v{}, child_link.id(), child_link);
+			return push(parent_node != root_ ? (**parent).first : lids_v{}, child_link.id(), child_link);
 		return {};
 	}
 
+	// returns parent index
 	auto make(const item_tag& child) -> item_index {
-		// obtain parent
+		// [NOTE] do we need this extra check?
 		auto parent_node = child.second.lock().owner();
-		if(!parent_node || parent_node == root_) return {{}, -1};
+		if(!parent_node || parent_node == root_) return none;
 
 		// child path have to consist at least of two parts
 		// if it contains one part, then parent is hidden root
 		const auto& child_path = child.first;
-		if(child_path.size() < 2) return {};
+		if(child_path.size() < 2) return none;
 
 		// extract grandparent, it's ID is child_path[-3]
-		auto grandpa_node = [&] {
-			if(child_path.size() < 3)
-				return root_;
-			else {
-				auto grandpa_link = deref_path(child_path.begin(), child_path.end() - 2, root_lnk_);
-				return grandpa_link ? data_node(grandpa_link) : sp_node{};
-			}
-		}();
-		if(!grandpa_node) return {};
+		auto grandpa_node = sp_node{};
+		if(child_path.size() < 3)
+			grandpa_node = root_;
+		else if(auto grandpa_link = deref_path(child_path.begin(), child_path.end() - 2, root_lnk_))
+			grandpa_node = data_node(grandpa_link);
+		if(!grandpa_node) return none;
 
 		// parent's ID is child_path[-2]
-		if(auto parent_link = grandpa_node->find(*(child_path.end() - 2)))
-			return {
-				&push(lids_v(child_path.begin(), child_path.end() - 1), parent_link),
-				grandpa_node->index(parent_link.id()).value_or(-1)
-			};
-		return {{}, -1};
+		auto parent_link = grandpa_node->find(*(child_path.end() - 2));
+		auto parent_path = lids_v(child_path.begin(), child_path.end() - 1);
+		if(auto parent_row = grandpa_node->index(parent_link.id()))
+			return { push(std::move(parent_path), parent_link), *parent_row };
+		else {
+			// remove incorrect path from tags cache
+			// [NOTE] disabled, because clients may rely on invalid entries
+			//pop(parent_path);
+			return none;
+		}
 	}
 
 	auto make(existing_tag child) -> item_index {
-		//bsout() << "*** parent()" << bs_end;
-		return child ? make(**child) : item_index{{}, -1};
+		//auto t = log::I("*** parent({}, {}) = [{}, {}]") <<
+		//	(child ? to_string((*child)->first) : "") <<
+		//	(child ? (*child)->second.lock().name(unsafe) : "");
+		return child ? make(**child) : none;
+		//if(res != none)
+		//	bsout() << t <<
+		//		(res == none ? "none" : to_string((*res.first)->first)) << res.second << bs_end;
+		//return res;
 	}
 
 	auto dump() const -> void {
+		//static auto state = idata_t{};
+		//if(idata_ == state) return;
+		//state = idata_;
+
 		for(const auto& [p, l] : idata_) {
 			auto L = l.lock();
 			if(!L) continue;
-			bsout() << "{} -> [{}, {}]" << to_string(p) << to_string(L.id()) << L.name(unsafe) << bs_end;
+			bsout() << "{} -> [{}]" << to_string(p) << L.name(unsafe) << bs_end;
 		}
 		bsout() << "====" << bs_end;
 	}
