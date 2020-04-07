0a2089c3514989477aa0ea9bce6a59fc608299e4
uentity <unientity@gmail.com>
2020-04-07 19:00:31 +0500

    actor_comon: implement anon request that's able to return callback result
    `anon_request_result()` returns worker actor handle. Caller can request
    callback result by sending `a_ack` message to it.

--- kernel/include/bs/actor_common.h
+++ kernel/include/bs/actor_common.h
@@ -9,14 +9,17 @@
 #pragma once
 
 #include <bs/common.h>
+#include <bs/atoms.h>
 #include <bs/timetypes.h>
 #include <bs/error.h>
 #include <bs/kernel/radio.h>
+#include <bs/detail/function_view.h>
 #include <bs/detail/tuple_utils.h>
 
 #include <caf/fwd.hpp>
 #include <caf/function_view.hpp>
 #include <caf/event_based_actor.hpp>
+#include <caf/stateful_actor.hpp>
 #include <caf/typed_behavior.hpp>
 
 #include <optional>
@@ -127,6 +130,45 @@ auto anon_request(Actor A, caf::duration timeout, bool high_priority, F f, Args&
 		.then(std::move(f));
 
 		return {};
+	});
+}
+
+/// @brief same as above but allows to receive result value returned from callback
+template<caf::spawn_options Os = caf::no_spawn_options, typename Actor, typename F, typename... Args>
+auto anon_request_result(Actor A, caf::duration timeout, bool high_priority, F f, Args&&... args) {
+	struct rstate {
+		// used to deliver result of `f()`
+		caf::response_promise res;
+	};
+	// spawn worker actor that will make request and invoke `f`
+	return kernel::radio::system().spawn<Os>([
+		high_priority, f = std::move(f), A = std::move(A), t = std::move(timeout),
+		args = std::make_tuple(std::forward<Args>(args)...)
+	] (caf::stateful_actor<rstate>* self) mutable -> caf::behavior {
+		// prepare response
+		self->state.res = self->make_response_promise();
+		// make request
+		using Finfo = detail::deduce_callable<std::remove_reference_t<F>>;
+		std::apply([self, high_priority, A = std::move(A), t = std::move(t)](auto&&... args) {
+			return high_priority ?
+				self->template request<high_prio>(A, t, std::forward<decltype(args)>(args)...) :
+				self->template request(A, t, std::forward<decltype(args)>(args)...);
+		}, std::move(args))
+		.then(std::function<typename Finfo::type>{
+			[res = self->state.res, f = std::move(f)](auto&&... xs) mutable {
+				if constexpr(!std::is_same_v<typename Finfo::result, void>)
+					res.deliver( f(std::forward<decltype(xs)>(xs)...) );
+				else {
+					f(std::forward<decltype(xs)>(xs)...);
+					res.deliver(caf::unit);
+				}
+			}
+		});
+
+		return {
+			// caller might send `a_ack` message to wait for callback invocation result
+			[=](a_ack) { return self->state.res; }
+		};
 	});
 }
 
