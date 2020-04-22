42adcc9c6b4d6ecdd28979f0e304b49ce40f166a
uentity <unientity@gmail.com>
2020-04-22 17:12:11 +0500

    meta: add `remove_cvref_t`, helpers to define perfect forwarding ctors

--- kernel/include/bs/meta.h
+++ kernel/include/bs/meta.h
@@ -29,4 +29,39 @@ constexpr decltype(auto) forward_as(typename std::remove_reference_t<T>& value) 
 template<class... Ts> struct overloaded : Ts... { using Ts::operator()...; };
 template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
+/// until we have C++20
+template<typename T> using remove_cvref_t = std::remove_cv_t<std::remove_reference_t<T>>;
+
+/// remove const/ref qualifiers in contrast to std counterparts
+template<typename T, typename U>
+inline constexpr auto is_same_uq = std::is_same_v<remove_cvref_t<T>, remove_cvref_t<U>>;
+
+template<typename T, typename U>
+inline constexpr auto is_base_of_uq = std::is_base_of_v<T, remove_cvref_t<U>>;
+
+/// check that first arg (cvref removed) from variadic pack is T (exact match or inherited from)
+template<typename T, typename A1 = std::add_pointer_t<T>, typename... Args>
+inline constexpr auto a1_is_t = [] {
+	if constexpr(std::is_fundamental_v<T>)
+		return std::is_same_v<T, remove_cvref_t<A1>>;
+	else
+		return is_base_of_uq<T, A1>;
+}();
+
+/// following can be used to narrow greedy perfect forward ctor by excluding copy & move ctors
+template<typename T, typename... Args>
+inline constexpr auto enable_pf_ctor_v = !(a1_is_t<T, Args...> && sizeof...(Args) == 1);
+
+template<typename T, typename... Args>
+using enable_pf_ctor = std::enable_if_t<enable_pf_ctor_v<T, Args...>>;
+
+/// assume that T's ctor var args pack is forwarded to initialize some other type U (for ex. base)
+template<typename T, typename U, typename... Args>
+inline constexpr auto enable_pf_ctor_to_v = enable_pf_ctor_v<T, Args...> &&
+	std::is_constructible_v<U, Args...>;
+
+template<typename T, typename U, typename... Args>
+using enable_pf_ctor_to = std::enable_if_t<enable_pf_ctor_v<T, U, Args...>>;
+
+
 } // eof blue_sky::meta
