44aac1433c148ff5c442804635ff11872e197041
uentity <unientity@gmail.com>
2020-04-23 16:51:23 +0500

    function_view: capture pure function pointers, extract it from stateless lambdas
    Add clause to `init()` that detects pure function pointer and stores it
    directly in `fn_`. Also function pointer is extracted from  stateless
    callables (lambdas) and stored in `fn_`.
    
    Similar thing was already implemented for `std::function` earlier, when
    target function pointer is captured into `function_view` directly
    (instead of address of std::function variable).
    
    All this tricks allows to *store* `function_view` instances in
    containers, etc, s.t. they can be used later *after* variable used to
    transfer function pointer to function_view is destroyed.
    [NOTE] This works only for stateless callbacks!
    
    Also use new meta enablers for shorter & prettier code.

--- kernel/include/bs/detail/function_view.h
+++ kernel/include/bs/detail/function_view.h
@@ -6,105 +6,35 @@
 /// This Source Code Form is subject to the terms of the Mozilla Public License,
 /// v. 2.0. If a copy of the MPL was not distributed with this file,
 /// You can obtain one at https://mozilla.org/MPL/2.0/
-//
-// [NOTE] Heavily modified version by Vittorio Romeo,
-// https://github.com/SuperV1234/vittorioromeo.info/blob/master/extra/passing_functions_to_functions/function_view.hpp
+///
+/// [NOTE] Credits to Simon Brand AKA TartanLlama & Vittorio Romeo
+/// https://github.com/SuperV1234/vittorioromeo.info/blob/master/extra/passing_functions_to_functions/function_view.hpp
 #pragma once
 
-#include <type_traits>
+#include "../meta.h"
 #include <functional>
 
 namespace blue_sky {
 
 /// [NOTE] `function_view` DO NOT extend lifetime of passed callable
+/// [WARNING] current impl will only work on platforms where function pointer is of same size as void*
 template<typename F> class function_view;
 
-template<typename R, typename... Args>
-class function_view<R (Args...)> {
-public:
-	using result_t = R;
-	using callable_t = R (Args...);
-	using pointer_t = R (*)(Args...);
-	using stdfn_t = std::function<callable_t>;
-
-private:
-	void* fn_;
-	R (*erased_fn_)(void*, Args...);
-
-	template<typename F>
-	inline constexpr auto init(F&& x) noexcept -> void {
-		constexpr auto is_compatible = std::is_invocable_r_v<R, F, Args...>;
-		static_assert(is_compatible, "Passed callable isn't compatible with target function signature");
-
-		if constexpr(is_compatible) {
-			if constexpr(std::is_same_v<std::decay_t<F>, stdfn_t>) {
-				fn_ = (void*)(*std::forward<F>(x).template target<pointer_t>());
-				erased_fn_ = [](void* fn, Args... xs) -> R {
-					return std::invoke(
-						reinterpret_cast<pointer_t>(fn), std::forward<Args>(xs)...
-					);
-				};
-			}
-			else {
-				fn_ = (void*)std::addressof(x);
-				erased_fn_ = [](void* fn, Args... xs) -> R {
-					return std::invoke(
-						*reinterpret_cast<std::add_pointer_t<F>>(fn), std::forward<Args>(xs)...
-					);
-				};
-			}
-		}
-	}
-
-public:
-	// construct from compatible callable
-	template<typename F, typename = std::enable_if_t<
-		!std::is_same_v<std::decay_t<F>, function_view>
-	>>
-	constexpr function_view(F&& x) noexcept { init(std::forward<F>(x)); }
-	// move, copy ctors & assignment ops are default
-	constexpr function_view(const function_view&) noexcept = default;
-	constexpr function_view(function_view&&) noexcept = default;
-	constexpr function_view& operator =(const function_view&) noexcept = default;
-	constexpr function_view& operator =(function_view&&) noexcept = default;
-
-	// assign from compatible callable
-	template<typename F, typename = std::enable_if_t<
-		!std::is_same_v<std::decay_t<F>, function_view>
-	>>
-	constexpr auto operator=(F&& x) noexcept -> function_view& {
-		init(std::forward<F>(x));
-		return *this;
-	}
-
-	constexpr auto swap(function_view& rhs) -> void {
-		std::swap(fn_, rhs.fn_);
-		std::swap(erased_fn_, rhs.erased_fn_);
-	}
-
-	// call stored callable with passed args
-	template<typename... Ts>
-	decltype(auto) operator()(Ts&&... xs) const
-		noexcept(noexcept(erased_fn_(fn_, std::forward<Ts>(xs)...)))
-	{
-		return erased_fn_(fn_, std::forward<Ts>(xs)...);
-	}
-};
-
-// swap support
-template<typename R, typename... Args>
-constexpr auto swap(function_view<R (Args...)>& lhs, function_view<R (Args...)>& rhs) -> void {
-	lhs.swap(rhs);
-}
-
-// deduction guides
 namespace detail {
+
+/// traits to detect function_view instance
+template<typename T> struct is_function_view : std::false_type {};
+template<typename F> struct is_function_view< function_view<F> > : std::true_type {};
+
+template<typename T> inline constexpr auto is_function_view_v =
+	is_function_view< meta::remove_cvref_t<T> >::value;
 
 template<typename F, typename = void> struct deduce_callable {
 	static_assert(std::is_invocable_v<F>, "Type isn't callable");
 };
 template<typename F> using deduce_callable_t = typename deduce_callable<std::remove_reference_t<F>>::type;
 
+/// treits for deducing callable signature
 template<typename R, typename... Args>
 struct deduce_callable<R (Args...), void> {
 	using type = R (Args...);
@@ -135,6 +65,98 @@ struct deduce_callable<F, std::void_t< decltype(&F::operator()) >> {
 
 } // eof blue_sky::detail
 
+/*-----------------------------------------------------------------------------
+ *  function_view is a drop-in replacement for argument accepting any callable type
+ *-----------------------------------------------------------------------------*/
+template<typename R, typename... Args>
+class function_view<R (Args...)> {
+public:
+	using result_t = R;
+	using callable_t = R (Args...);
+	using pointer_t = R (*)(Args...);
+	using stdfn_t = std::function<callable_t>;
+
+private:
+	void* fn_;
+	R (*erased_fn_)(void*, Args...);
+
+	template<typename X> friend class function_view;
+
+	template<typename F>
+	inline constexpr auto init(F&& x) noexcept -> void {
+		static_assert(
+			std::is_invocable_r_v<R, F, Args...>,
+			"Passed callable isn't compatible with signature of this function_view"
+		);
+
+		using Fpure = meta::remove_cvref_t<F>;
+		if constexpr(std::is_pointer_v<Fpure>) {
+			// store function pointer directly in fn_
+			fn_ = (void*)reinterpret_cast<Fpure>(x);
+			erased_fn_ = [](void* fn, Args... xs) -> R {
+				return std::invoke( (Fpure)fn, std::forward<Args>(xs)... );
+			};
+		}
+		else if constexpr(std::is_same_v<Fpure, stdfn_t> || std::is_convertible_v<Fpure, pointer_t>) {
+			if constexpr(std::is_same_v<Fpure, stdfn_t>)
+				// extract target function pointer from std::function
+				fn_ = (void*)(*x.template target<pointer_t>());
+			else
+				// convert stateless callable (lambda) to function pointer
+				fn_ = (void*)static_cast<pointer_t>(x);
+			erased_fn_ = [](void* fn, Args... xs) -> R {
+				return std::invoke( (pointer_t)fn, std::forward<Args>(xs)... );
+			};
+		}
+		else {
+			// generic case that can capture anything, but needs `x` to be alive
+			fn_ = (void*)std::addressof(x);
+			erased_fn_ = [](void* fn, Args... xs) -> R {
+				return std::invoke(
+					*reinterpret_cast<std::add_pointer_t<F>>(fn), std::forward<Args>(xs)...
+				);
+			};
+		}
+	}
+
+public:
+	// construct from compatible callable
+	template<typename F, typename = meta::enable_pf_ctor<function_view, F>>
+	constexpr function_view(F&& x) noexcept { init(std::forward<F>(x)); }
+	// move, copy ctors & assignment ops are default
+	constexpr function_view(const function_view&) noexcept = default;
+	constexpr function_view(function_view&&) noexcept = default;
+	constexpr function_view& operator =(const function_view&) noexcept = default;
+	constexpr function_view& operator =(function_view&&) noexcept = default;
+
+	// assign from compatible callable
+	template<typename F, typename = meta::enable_pf_ctor<function_view, F>>
+	constexpr auto operator=(F&& x) noexcept -> function_view& {
+		init(std::forward<F>(x));
+		return *this;
+	}
+
+	constexpr auto swap(function_view& rhs) -> void {
+		std::swap(fn_, rhs.fn_);
+		std::swap(erased_fn_, rhs.erased_fn_);
+	}
+
+	// call stored callable with passed args
+	template<typename... Ts>
+	decltype(auto) operator()(Ts&&... xs) const
+		noexcept(noexcept(erased_fn_(fn_, std::forward<Ts>(xs)...)))
+	{
+		return erased_fn_(fn_, std::forward<Ts>(xs)...);
+	}
+};
+
+// swap support
+template<typename R, typename... Args>
+constexpr auto swap(function_view<R (Args...)>& lhs, function_view<R (Args...)>& rhs) -> void {
+	lhs.swap(rhs);
+}
+
+// deduction guides
 template<typename F>
 function_view(F) -> function_view< detail::deduce_callable_t<F> >;
 
