uentity/bluesky

commit 67d3ca256c27d1cb85e5a9de3b5b5346f1fb6a8d

uuid: switch to new BS UUIDs source

kernel/src/serialize/tree_fs_output.cpp
@@ -21,8 +21,6 @@
 #include <bs/serialize/tree.h>
 #include <bs/serialize/cafbind.h>
 
-#include <boost/uuid/uuid_io.hpp>
-
 #include <cereal/types/vector.hpp>
 #include <fmt/format.h>
 #include <fmt/ostream.h>
@@ -129,8 +127,7 @@ struct tree_fs_output::impl : detail::file_heads_manager<true> {
 			bool res_exists = false;
 			if(auto er = error::eval([&] {
 				for(int i = 0; (res_exists = fs::exists(res_fname)) && i < 100000; ++i) {
-					res_fname = start_fname;
-					res_fname += "_" + std::to_string(i) + fmt_ext;
+					res_fname = fmt::format("{}_{}{}", start_fname, i, fmt_ext);
 				}
 			})) return tl::make_unexpected(std::move(er));
 

kernel/include/bs/defaults.h
@@ -9,8 +9,7 @@
 
 #include "common.h"
 #include "timetypes.h"
-
-#include <boost/uuid/uuid.hpp>
+#include "uuid.h"
 
 NAMESPACE_BEGIN(blue_sky::defaults)
 
@@ -28,7 +27,7 @@ NAMESPACE_END(kernel)
 
 NAMESPACE_BEGIN(tree)
 
-BS_API extern const boost::uuids::uuid nil_uid;
+BS_API extern const uuid nil_uid;
 BS_API extern const std::string nil_oid;
 BS_API extern const char* nil_grp_id;
 BS_API extern const char* nil_link_name;

kernel/include/bs/tree/common.h
@@ -9,11 +9,10 @@
 #pragma once
 
 #include "../common.h"
+#include "../uuid.h"
 #include "../detail/enumops.h"
 #include "../detail/function_view.h"
 #include "inode.h"
-
-#include <boost/uuid/uuid.hpp>
 
 NAMESPACE_BEGIN(blue_sky::tree)
 
@@ -56,26 +55,9 @@ enum class InsertPolicy {
 };
 
 /// link's unique ID type
-using lid_type = boost::uuids::uuid;
+using lid_type = uuid;
 /// function that modifies link's pointee
 using data_modificator_f = objbase::modificator_f;
-
-/// convert string to lid_type eating exceptions
-/// decode error will be logged
-BS_API auto to_uuid(std::string_view s) noexcept -> std::optional<lid_type>;
-
-/// apply given function to successfully decoded input, return `defval` as fallback value
-template<typename F>
-auto if_uuid(std::string_view s, F&& f, std::invoke_result_t<F, lid_type> defval)
-noexcept(noexcept( f(std::declval<lid_type>()) )) {
-	if(auto u = to_uuid(s)) return f(*u);
-	return defval;
-}
-/// overload for void
-inline auto if_uuid(std::string_view s, function_view< void (lid_type) > f)
-noexcept(noexcept( f(std::declval<lid_type>()) )) -> void {
-	if(auto u = to_uuid(s)) f(*u);
-}
 
 /// can be passed as callback that does nothing
 inline constexpr auto noop = [](auto&&...) {};

kernel/src/defaults.cpp
@@ -12,7 +12,6 @@
 #include <bs/kernel/config.h>
 
 #include <boost/uuid/nil_generator.hpp>
-#include <boost/uuid/uuid_io.hpp>
 
 NAMESPACE_BEGIN(blue_sky)
 NAMESPACE_BEGIN(defaults)
@@ -31,7 +30,7 @@ NAMESPACE_END(kernel)
 
 NAMESPACE_BEGIN(tree)
 
-const boost::uuids::uuid nil_uid = boost::uuids::nil_uuid();
+const uuid nil_uid = boost::uuids::nil_uuid();
 const std::string nil_oid = to_string(nil_uid);
 const char* nil_grp_id = "__bs_nil_group__";
 const char* nil_link_name = "__bs_nil_link__";

kernel/src/error.cpp
@@ -11,13 +11,12 @@
 #include <bs/tree/errors.h>
 #include <bs/misc.h>
 #include <bs/log.h>
+#include <bs/uuid.h>
 #include <bs/kernel/tools.h>
 #include <bs/kernel/misc.h>
 #include <bs/kernel/radio.h>
 
 #include <fmt/format.h>
-#include <boost/uuid/random_generator.hpp>
-#include <boost/uuid/uuid_io.hpp>
 
 #include <ostream>
 #include <string_view>
@@ -43,7 +42,7 @@ struct cat_registry {
 	static auto self() -> cat_registry& {
 		static cat_registry& self = []() -> cat_registry& {
 			// generate random key
-			auto& kstorage = kernel::idx_key_storage(to_string( boost::uuids::random_generator()() ));
+			auto& kstorage = kernel::idx_key_storage(to_string(gen_uuid()));
 			auto r = kstorage.insert_element(0, cat_registry{});
 			if(!r.first) throw error("Failed to instantiate error categories registry in kernel storage!");
 			return *r.first;

kernel/src/kernel/python_subsyst.cpp
@@ -17,8 +17,6 @@
 #include "plugins_subsyst.h"
 #include "radio_subsyst.h"
 
-#include <boost/uuid/uuid_io.hpp>
-
 #include <pybind11/functional.h>
 
 #include <functional>

kernel/src/kernel/tools.cpp
@@ -12,7 +12,6 @@
 #include <bs/kernel/types_factory.h>
 #include <bs/tree/node.h>
 #include <sstream>
-#include <boost/uuid/uuid_io.hpp>
 
 #ifdef _DEBUG
 #ifdef _WIN32

kernel/src/objbase.cpp
@@ -8,25 +8,16 @@
 /// You can obtain one at https://mozilla.org/MPL/2.0/
 
 #include <bs/objbase.h>
+#include <bs/uuid.h>
 #include <bs/kernel/types_factory.h>
 #include <bs/tree/errors.h>
 #include <bs/tree/inode.h>
-
-#include <boost/uuid/uuid_generators.hpp>
-#include <boost/uuid/uuid_io.hpp>
 
 // -----------------------------------------------------
 // Implementation of class: object_base
 // -----------------------------------------------------
 
 NAMESPACE_BEGIN(blue_sky)
-
-namespace {
-
-// global random UUID generator for BS objects
-static boost::uuids::random_generator gen;
-
-} // eof hidden namespace
 
 objbase::objbase(std::string custom_oid)
 	: objbase(false, custom_oid)
@@ -35,14 +26,14 @@ objbase::objbase(std::string custom_oid)
 }
 
 objbase::objbase(bool is_node, std::string custom_oid)
-	: id_(custom_oid.size() ? std::move(custom_oid) : boost::uuids::to_string(gen())),
+	: id_(custom_oid.size() ? std::move(custom_oid) : to_string(gen_uuid())),
 	is_node_(is_node)
 {
 	start_engine();
 }
 
 objbase::objbase(const objbase& obj)
-	: enable_shared_from_this(obj), id_(boost::uuids::to_string(gen())), is_node_(obj.is_node_)
+	: enable_shared_from_this(obj), id_(to_string(gen_uuid())), is_node_(obj.is_node_)
 {
 	start_engine();
 }

commit 5f8dc55cda51b69fcbad840d446381cbaa792259

serial/tree_fs_output: fix regression introduced by commit 67d3ca256c27d1cb85e5a9de3b5b5346f1fb6a8d

kernel/src/serialize/tree_fs_output.cpp
@@ -127,7 +127,8 @@ struct tree_fs_output::impl : detail::file_heads_manager<true> {
 			bool res_exists = false;
 			if(auto er = error::eval([&] {
 				for(int i = 0; (res_exists = fs::exists(res_fname)) && i < 100000; ++i) {
-					res_fname = fmt::format("{}_{}{}", start_fname, i, fmt_ext);
+					res_fname = start_fname;
+					res_fname += "_" + std::to_string(i) + fmt_ext;
 				}
 			})) return tl::make_unexpected(std::move(er));
 