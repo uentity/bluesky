3c3471f16c32c1ad04d1ce1f68e47c224bc8b165
Alexander Gagarin @uentity <unientity@gmail.com>
2020-11-21 00:40:28 +0500

    enumops: allow both ways to enable arithmetic ops for enum
    1. With original one - by providing specialization of
       `enable_enumops<T>` struct.
    
    2. By `using namespace allow_enumops`.

--- kernel/include/bs/detail/enumops.h
+++ kernel/include/bs/detail/enumops.h
@@ -11,66 +11,83 @@
 #include <type_traits>
 #include <utility>
 
+#define BS_ALLOW_ENUMOPS(...) \
+template<> struct blue_sky::enumops_enabled< blue_sky::__VA_ARGS__ > : std::true_type {};
+
 namespace blue_sky {
+
+/// provide specialization of this to enable ops	
+template<typename T> struct enumops_enabled : std::false_type {};
+
 namespace detail {
 
-template<typename T, typename R = void>
-using enable_if_enum_t = std::enable_if_t<std::is_enum_v<std::decay_t<T>>, R>;
+template<typename T>
+inline constexpr auto enumops_enabled_v = enumops_enabled<T>::value;
+
+template<typename T>
+static constexpr bool enumops_allowed(...) { return false; }
+
+template<typename T>
+static constexpr auto enumops_allowed(int) -> std::enable_if_t<enumops_enabled_v<T>, bool> {
+	return std::is_enum_v<T>;
+}
 
 } // eof namespace blue_sky::detail
+
+template<bool Force, typename T, typename R = void>
+using if_enumops_allowed = std::enable_if_t<Force | detail::enumops_allowed<T>(0), R>;
 
 ///////////////////////////////////////////////////////////////////////////////
 //  extract enum underlying type value
 //
 // allows to optionally specify return type
-template<typename R = void, typename T, typename = detail::enable_if_enum_t<T>>
+template<typename R = void, typename T, typename = std::enable_if_t<std::is_enum_v<T>>>
 constexpr auto enumval(T a) {
 	using R_ = std::conditional_t<std::is_same_v<R, void>, std::underlying_type_t<T>, R>;
 	return static_cast<R_>(a);
 }
 
-namespace allow_enumops {
 ///////////////////////////////////////////////////////////////////////////////
 //  unary ops
 //
-template<typename T>
-constexpr auto operator~(T a) -> detail::enable_if_enum_t<T, T> {
+template<bool Force = false, typename T>
+constexpr auto operator~(T a) -> if_enumops_allowed<Force, T, T> {
 	return T(~enumval(a));
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 //  binary enum - enum ops
 //
-template<typename T>
-constexpr auto operator|(T a, T b) -> detail::enable_if_enum_t<T, T> {
+template<bool Force = false, typename T>
+constexpr auto operator|(T a, T b) -> if_enumops_allowed<Force, T, T> {
 	return static_cast<T>(enumval(a) | enumval(b));
 }
 
-template<typename T>
-constexpr auto operator&(T a, T b) -> detail::enable_if_enum_t<T, T> {
+template<bool Force = false, typename T>
+constexpr auto operator&(T a, T b) -> if_enumops_allowed<Force, T, T> {
 	return static_cast<T>(enumval(a) & enumval(b));
 }
 
-template<typename T>
-constexpr auto operator^(T a, T b) -> detail::enable_if_enum_t<T, T> {
+template<bool Force = false, typename T>
+constexpr auto operator^(T a, T b) -> if_enumops_allowed<Force, T, T> {
 	return static_cast<T>(enumval(a) ^ enumval(b));
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 //  binary enum - underlying type ops
 //
-template<typename T>
-constexpr auto operator|(T a, std::underlying_type_t<T> b) -> detail::enable_if_enum_t<T, T> {
+template<bool Force = false, typename T>
+constexpr auto operator|(T a, std::underlying_type_t<T> b) -> if_enumops_allowed<Force, T, T> {
 	return static_cast<T>(enumval(a) | b);
 }
 
-template<typename T>
-constexpr auto operator&(T a, std::underlying_type_t<T> b) -> detail::enable_if_enum_t<T, T> {
+template<bool Force = false, typename T>
+constexpr auto operator&(T a, std::underlying_type_t<T> b) -> if_enumops_allowed<Force, T, T> {
 	return static_cast<T>(enumval(a) & b);
 }
 
-template<typename T>
-constexpr auto operator^(T a, std::underlying_type_t<T> b) -> detail::enable_if_enum_t<T, T> {
+template<bool Force = false, typename T>
+constexpr auto operator^(T a, std::underlying_type_t<T> b) -> if_enumops_allowed<Force, T, T> {
 	return static_cast<T>(enumval(a) ^ b);
 }
 
@@ -78,19 +95,91 @@ constexpr auto operator^(T a, std::underlying_type_t<T> b) -> detail::enable_if_
 //  in-place lhs modification
 //
 template<typename T, typename U>
-constexpr auto operator|=(T& a, U b) -> detail::enable_if_enum_t<T, T&> {
+constexpr auto operator|=(T& a, U b) -> if_enumops_allowed<false, T, T&> {
 	return a = a | b;
 }
 
 template<typename T, typename U>
-constexpr auto operator&=(T& a, U b) -> detail::enable_if_enum_t<T, T&> {
+constexpr auto operator&=(T& a, U b) -> if_enumops_allowed<false, T, T&> {
 	return a = a & b;
 }
 
 template<typename T, typename U>
-constexpr auto operator^=(T& a, U b) -> detail::enable_if_enum_t<T, T&> {
+constexpr auto operator^=(T& a, U b) -> if_enumops_allowed<false, T, T&> {
 	return a = a ^ b;
 }
 
-}} /* namespace blue_sky */
+/*-----------------------------------------------------------------------------
+ *  add `using namespace allow_enumops` to enable ops for all enums in a scope
+ *-----------------------------------------------------------------------------*/
+namespace allow_enumops {
+
+template<typename T, typename R = void>
+using if_enumops_allowed = std::enable_if_t<!detail::enumops_allowed<T>(0), R>;
+
+///////////////////////////////////////////////////////////////////////////////
+//  unary ops
+//
+template<bool Force = false, typename T>
+constexpr auto operator~(T a) -> if_enumops_allowed<T, T> {
+	return blue_sky::operator~<true>(a);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//  binary enum - enum ops
+//
+template<bool Force = false, typename T>
+constexpr auto operator|(T a, T b) -> if_enumops_allowed<T, T> {
+	return blue_sky::operator|<true>(a, b);
+}
+
+template<bool Force = false, typename T>
+constexpr auto operator&(T a, T b) -> if_enumops_allowed<T, T> {
+	return blue_sky::operator&<true>(a, b);
+}
+
+template<bool Force = false, typename T>
+constexpr auto operator^(T a, T b) -> if_enumops_allowed<T, T> {
+	return blue_sky::operator^<true>(a, b);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//  binary enum - underlying type ops
+//
+template<bool Force = false, typename T>
+constexpr auto operator|(T a, std::underlying_type_t<T> b) -> if_enumops_allowed<T, T> {
+	return blue_sky::operator|<true>(a, b);
+}
+
+template<bool Force = false, typename T>
+constexpr auto operator&(T a, std::underlying_type_t<T> b) -> if_enumops_allowed<T, T> {
+	return blue_sky::operator&<true>(a, b);
+}
+
+template<bool Force = false, typename T>
+constexpr auto operator^(T a, std::underlying_type_t<T> b) -> if_enumops_allowed<T, T> {
+	return blue_sky::operator^<true>(a, b);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//  in-place lhs modification
+//
+template<typename T, typename U>
+constexpr auto operator|=(T& a, U b) -> if_enumops_allowed<T, T&> {
+	return a = a | b;
+}
+
+template<typename T, typename U>
+constexpr auto operator&=(T& a, U b) -> if_enumops_allowed<T, T&> {
+	return a = a & b;
+}
+
+template<typename T, typename U>
+constexpr auto operator^=(T& a, U b) -> if_enumops_allowed<T, T&> {
+	return a = a ^ b;
+}
+
+} // eof blue_sky::allow_enumops
+
+} /* namespace blue_sky */
 
